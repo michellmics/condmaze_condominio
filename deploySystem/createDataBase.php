4da28cce49bf7145d10d2290b7e507b3db48f699
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-14 23:31:10 +0000

    Deploy

--- deploySystem/createDataBase.php
+++ deploySystem/createDataBase.php
@@ -4,6 +4,7 @@ $cpanel_user = 'inartcom'; // Usuário do cPanel
 $cpanel_token = 'WNKQZAKMU8ZP0C6EW82PW67ZMZLTUUC0'; // Token gerado no cPanel
 $cpanel_host = 'https://inart.com.br:2083'; // URL do cPanel (substitua pelo domínio do seu cPanel)
 
+
 // Dados do banco de dados
 $database_name = 'seubd';
 $database_user = 'seuusuario';
@@ -15,9 +16,12 @@ $headers = [
     'Content-Type: application/json'
 ];
 
-// Função para chamar a API do cPanel
-function call_cpanel_api($url, $headers) {
-    $ch = curl_init($url);
+// Função para chamar a API UAPI do cPanel
+function call_uapi($cpanel_host, $endpoint, $params, $headers) {
+    $url = $cpanel_host . '/execute/' . $endpoint;
+    $query = http_build_query($params);
+
+    $ch = curl_init($url . '?' . $query);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     $response = curl_exec($ch);
@@ -31,63 +35,33 @@ function call_cpanel_api($url, $headers) {
 }
 
 // 1. Criar o banco de dados
-$create_db_url = "$cpanel_host/json-api/cpanel?cpanel_jsonapi_user=$cpanel_user&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Mysql&cpanel_jsonapi_func=create_database&name=$database_name";
-$response = call_cpanel_api($create_db_url, $headers);
+$response = call_uapi($cpanel_host, 'Mysql/create_database', ['name' => $database_name], $headers);
 
-if (isset($response['cpanelresult']['error'])) {
-    die("Erro ao criar o banco de dados: " . $response['cpanelresult']['error']);
+if ($response['status'] !== 1) {
+    die("Erro ao criar o banco de dados: " . $response['errors'][0]);
 }
 echo "Banco de dados criado com sucesso!\n";
 
-// 2. Criar o usuário
-$create_user_url = "$cpanel_host/json-api/cpanel?cpanel_jsonapi_user=$cpanel_user&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Mysql&cpanel_jsonapi_func=create_user&name=$database_user&password=$database_password";
-$response = call_cpanel_api($create_user_url, $headers);
+// 2. Criar o usuário do banco de dados
+$response = call_uapi($cpanel_host, 'Mysql/create_user', [
+    'name' => $database_user,
+    'password' => $database_password
+], $headers);
 
-if (isset($response['cpanelresult']['error'])) {
-    die("Erro ao criar o usuário do banco de dados: " . $response['cpanelresult']['error']);
+if ($response['status'] !== 1) {
+    die("Erro ao criar o usuário do banco de dados: " . $response['errors'][0]);
 }
 echo "Usuário criado com sucesso!\n";
 
-// 3. Associar o usuário ao banco
-$add_user_to_db_url = "$cpanel_host/json-api/cpanel?cpanel_jsonapi_user=$cpanel_user&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Mysql&cpanel_jsonapi_func=set_privileges_on_database&user=$database_user&database=$database_name&privileges=ALL";
-$response = call_cpanel_api($add_user_to_db_url, $headers);
+// 3. Conceder permissões ao usuário no banco de dados
+$response = call_uapi($cpanel_host, 'Mysql/set_privileges_on_database', [
+    'user' => $database_user,
+    'database' => $database_name,
+    'privileges' => 'ALL PRIVILEGES'
+], $headers);
 
-if (isset($response['cpanelresult']['error'])) {
-    die("Erro ao associar o usuário ao banco de dados: " . $response['cpanelresult']['error']);
-}
-echo "Usuário associado ao banco com sucesso!\n";
-?>
-
-
-// Autenticação no cPanel
-$headers = [
-    'Authorization: Basic ' . base64_encode($cpanel_user . ':' . $cpanel_pass)
-];
-
-// Criar o banco de dados
-$create_db_url = "$cpanel_host/json-api/cpanel?cpanel_jsonapi_user=$cpanel_user&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Mysql&cpanel_jsonapi_func=create_database&name=$database_name";
-$response = file_get_contents($create_db_url, false, stream_context_create(['http' => ['header' => $headers]]));
-
-if ($response === false) {
-    die("Erro ao criar o banco de dados.");
-}
-echo "Banco de dados criado com sucesso!\n";
-
-// Criar o usuário
-$create_user_url = "$cpanel_host/json-api/cpanel?cpanel_jsonapi_user=$cpanel_user&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Mysql&cpanel_jsonapi_func=create_user&name=$database_user&password=$database_password";
-$response = file_get_contents($create_user_url, false, stream_context_create(['http' => ['header' => $headers]]));
-
-if ($response === false) {
-    die("Erro ao criar o usuário do banco de dados.");
-}
-echo "Usuário criado com sucesso!\n";
-
-// Associar o usuário ao banco
-$add_user_to_db_url = "$cpanel_host/json-api/cpanel?cpanel_jsonapi_user=$cpanel_user&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Mysql&cpanel_jsonapi_func=set_privileges_on_database&user=$database_user&database=$database_name&privileges=ALL";
-$response = file_get_contents($add_user_to_db_url, false, stream_context_create(['http' => ['header' => $headers]]));
-
-if ($response === false) {
-    die("Erro ao associar o usuário ao banco de dados.");
+if ($response['status'] !== 1) {
+    die("Erro ao conceder permissões ao usuário: " . $response['errors'][0]);
 }
 echo "Usuário associado ao banco com sucesso!\n";
 ?>
