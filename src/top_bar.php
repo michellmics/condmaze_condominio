7e0a6d546a179e67e2afa63bab9e19b4f9d3354f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 18:17:18 +0000

    Deploy

--- src/top_bar.php
+++ src/top_bar.php
@@ -35,10 +35,10 @@
                         <!-- Logo light -->
                         <a href="index.html" class="logo-light">
                             <span class="logo-lg">
-                                <img src="../../assets/images/logo.png" alt="logo">
+                                <img src="./../img/logo_128x32_white.png" alt="logo">
                             </span>
                             <span class="logo-sm">
-                                <img src="../../assets/images/logo-sm.png" alt="small logo">
+                                <img src="./../img/logo_128x32_white.png" alt="small logo">
                             </span>
                         </a>
 
