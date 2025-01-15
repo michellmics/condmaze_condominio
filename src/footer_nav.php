280244503521b3060e066b304d830bbfb679023f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 11:06:47 +0000

    Deploy

--- src/footer_nav.php
+++ src/footer_nav.php
@@ -79,15 +79,11 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6">               
+            <div class="col-md-12">               
                 <a href="https://codemaze.com.br" target="_blank"><b>Codemaze</b></a> - Soluções de Mkt e Software | 
                 <b><font color="red"><?php echo $_SESSION['user_nivelacesso']; ?></font></b> | 
             </div>
-            <div class="col-md-6">
-                <div class="text-md-end footer-links d-none d-md-block">
-                    <a href="javascript: void(0);">Webmail</a>
-                </div>
-            </div>
+
         </div>
     </div>
 </footer>
