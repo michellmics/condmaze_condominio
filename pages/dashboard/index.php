7b1ad43e114e958914c4e7678e174ce593e58515
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 23:04:40 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -161,7 +161,7 @@
                                     <div class="card widget-flat">
                                         <div class="card-body">
                                             <div class="float-end">
-                                                
+                                                <i class="mdi mdi-currency-usd widget-icon bg-success-lighten text-success"></i>
                                             </div>
                                             <h5 class="text-muted fw-normal mt-0" title="Number of Customers">Receita</h5>
                                             <h3 class="mt-3 mb-3">R$112.025,23</h3>
@@ -195,7 +195,7 @@
                                     <div class="card widget-flat">
                                         <div class="card-body">
                                             <div class="float-end">
-                                                <i class="mdi mdi-currency-usd widget-icon bg-success-lighten text-success"></i>
+                                                <i class="mdi mdi-pulse widget-icon"></i>
                                             </div>
                                             <h5 class="text-muted fw-normal mt-0" title="Average Revenue">Fundo de Reserva</h5>
                                             <h3 class="mt-3 mb-3">R$100.369,23</h3>
@@ -211,7 +211,7 @@
                                     <div class="card widget-flat">
                                         <div class="card-body">
                                             <div class="float-end">
-                                                <i class="mdi mdi-pulse widget-icon"></i>
+                                                <i class="mdi mdi-account-multiple widget-icon"></i>
                                             </div>
                                             <h5 class="text-muted fw-normal mt-0" title="Growth">Inadimplência</h5>
                                             <h3 class="mt-3 mb-3">30.56%</h3>
