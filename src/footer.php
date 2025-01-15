9a234b0ba34f06aba15b06fe479beefc3c4fa072
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 17:38:58 +0000

    Deploy

--- src/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="footerNew"> 
-  <!-- Conteúdo abaixo do marquee -->
-  <div class="footer-info">
-    <a href="https://codemaze.com.br" target="_blank"><b>Codemaze</b></a> - Soluções de Mkt e Software
-  </div>
-</footer>
