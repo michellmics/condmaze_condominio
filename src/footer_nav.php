4761dd3aae3977f8f335625abcc1d0ebb090a22e
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 10:58:02 +0000

    Deploy

--- src/footer_nav.php
+++ src/footer_nav.php
@@ -13,7 +13,9 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
-                <script>document.write(new Date().getFullYear())</script> © Hyper - Coderthemes.com
+               
+                <a href="https://codemaze.com.br" target="_blank"><b>Codemaze</b></a> - Soluções de Mkt e Software | 
+                <b><font color="red"><?php echo $_SESSION['user_nivelacesso']; ?></font></b> | 
             </div>
             <div class="col-md-6">
                 <div class="text-md-end footer-links d-none d-md-block">
