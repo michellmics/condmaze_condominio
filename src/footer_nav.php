70823a6a592e8fd5a656cf495d5d7bda167ee499
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 11:01:28 +0000

    Deploy

--- src/footer_nav.php
+++ src/footer_nav.php
@@ -62,8 +62,22 @@
     <span><?php echo "texxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxx"; ?></span>
   </div>
 <footer class="footer">
-      <!-- Marquee com o conteúdo rolante -->
-
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+               
+                <a href="https://codemaze.com.br" target="_blank"><b>Codemaze</b></a> - Soluções de Mkt e Software | 
+                <b><font color="red"><?php echo $_SESSION['user_nivelacesso']; ?></font></b> | 
+            </div>
+            <div class="col-md-6">
+                <div class="text-md-end footer-links d-none d-md-block">
+                    <a href="javascript: void(0);">Webmail</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<footer class="footer">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
