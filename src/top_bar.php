e11999b4c393384133d1a4c03f8092e942159226
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 17:50:30 +0000

    Deploy

--- src/top_bar.php
+++ src/top_bar.php
@@ -75,6 +75,12 @@
 
                 <ul class="topbar-menu d-flex align-items-center gap-3">
 
+                    <li class="d-none d-sm-inline-block">
+                        <div class="nav-link" id="light-dark-mode" data-bs-toggle="tooltip" data-bs-placement="left" title="Theme Mode">
+                            <i class="ri-moon-line font-22"></i>
+                        </div>
+                    </li>
+
                     <li class="dropdown">
                         <a class="nav-link dropdown-toggle arrow-none nav-user px-4" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
                             <span class="account-user-avatar">
