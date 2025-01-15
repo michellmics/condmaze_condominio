b91312cccdbce7679b36921b0b6b21d0a4590014
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 22:14:26 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -149,11 +149,11 @@
                                             <div class="float-end">
                                                 <i class="mdi mdi-cart-plus widget-icon bg-success-lighten text-success"></i>
                                             </div>
-                                            <h5 class="text-muted fw-normal mt-0" title="Number of Orders">Orders</h5>
-                                            <h3 class="mt-3 mb-3">5,543</h3>
+                                            <h5 class="text-muted fw-normal mt-0" title="Number of Orders">Contas a Pagar</h5>
+                                            <h3 class="mt-3 mb-3">R$110.235,25</h3>
                                             <p class="mb-0 text-muted">
                                                 <span class="text-danger me-2"><i class="mdi mdi-arrow-down-bold"></i> 1.08%</span>
-                                                <span class="text-nowrap">Since last month</span>
+                                                <span class="text-nowrap">Último mês</span>
                                             </p>
                                         </div> <!-- end card-body-->
                                     </div> <!-- end card-->
@@ -167,11 +167,11 @@
                                             <div class="float-end">
                                                 <i class="mdi mdi-currency-usd widget-icon bg-success-lighten text-success"></i>
                                             </div>
-                                            <h5 class="text-muted fw-normal mt-0" title="Average Revenue">Revenue</h5>
-                                            <h3 class="mt-3 mb-3">$6,254</h3>
+                                            <h5 class="text-muted fw-normal mt-0" title="Average Revenue">Fundo de Reserva</h5>
+                                            <h3 class="mt-3 mb-3">R$100.369,23</h3>
                                             <p class="mb-0 text-muted">
                                                 <span class="text-danger me-2"><i class="mdi mdi-arrow-down-bold"></i> 7.00%</span>
-                                                <span class="text-nowrap">Since last month</span>
+                                                <span class="text-nowrap">Mês Atual</span>
                                             </p>
                                         </div> <!-- end card-body-->
                                     </div> <!-- end card-->
@@ -183,11 +183,11 @@
                                             <div class="float-end">
                                                 <i class="mdi mdi-pulse widget-icon"></i>
                                             </div>
-                                            <h5 class="text-muted fw-normal mt-0" title="Growth">Growth</h5>
-                                            <h3 class="mt-3 mb-3">+ 30.56%</h3>
+                                            <h5 class="text-muted fw-normal mt-0" title="Growth">Inadimplência</h5>
+                                            <h3 class="mt-3 mb-3">30.56%</h3>
                                             <p class="mb-0 text-muted">
-                                                <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> 4.87%</span>
-                                                <span class="text-nowrap">Since last month</span>
+                                                <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> 30.87%</span>
+                                                <span class="text-nowrap">Último mês</span>
                                             </p>
                                         </div> <!-- end card-body-->
                                     </div> <!-- end card-->
