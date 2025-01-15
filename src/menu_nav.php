2afb5b9ada53c9fe3afc77dcbfa914e0f1dc67ee
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 11:10:44 +0000

    Deploy

--- src/menu_nav.php
+++ src/menu_nav.php
@@ -76,15 +76,8 @@
                         <ul class="navbar-nav">
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-dashboards" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="uil-dashboard"></i>Dashboards <div class="arrow-down"></div>
+                                    <i class="uil-window"></i>Início <div class="arrow-down"></div>
                                 </a>
-                                <div class="dropdown-menu" aria-labelledby="topnav-dashboards">
-                                    <a href="dashboard-analytics.html" class="dropdown-item">Analytics</a>
-                                    <a href="index.html" class="dropdown-item">Ecommerce</a>
-                                    <a href="dashboard-projects.html" class="dropdown-item">Projects</a>
-                                    <a href="dashboard-crm.html" class="dropdown-item">CRM</a>
-                                    <a href="dashboard-wallet.html" class="dropdown-item">E-Wallet</a>
-                                </div>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-apps" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
