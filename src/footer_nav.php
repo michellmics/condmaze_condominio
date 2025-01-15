c8fe8ead65054b0c39d71c82ed7aaa4420592fa5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 18:06:33 +0000

    Deploy

--- src/footer_nav.php
+++ src/footer_nav.php
@@ -1,3 +1,22 @@
+<style>
+        html, body {
+        height: 100%; /* Garante que a altura da página seja de 100% da janela */
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+    }
+    
+    .container-fluid {
+        flex: 1; /* O conteúdo principal ocupará o espaço disponível */
+    }
+    
+    .footer {
+        background-color: #f8f9fa; /* Cor de fundo opcional */
+        text-align: center; /* Centraliza o texto */
+        padding: 10px 0; /* Espaçamento interno */
+        position: relative; /* Garante que o rodapé fique no final */
+    }
+</style>
 <footer class="footer">
     <div class="container-fluid">
         <div class="row">
