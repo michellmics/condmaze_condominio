ab29cb80e5dd11cb10140b01bb6b995eeefdcd6f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 22:56:12 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -142,7 +142,7 @@
                                         </div>
                                     </form>
                                 </div>
-                                <h4 class="page-title"></h4> 
+                                <h4 class="page-title">Indicadores</h4> 
                             </div>
                         </div>
                     </div>
