26472524eae2c7010c341bae30536a6c588aee7d
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 22:41:26 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -120,13 +120,6 @@
                                                 <i class="mdi mdi-calendar-range font-13"></i>
                                             </span>
                                         </div>
-
-                                        <a href="javascript: void(0);" class="btn btn-primary ms-2">
-                                            <i class="mdi mdi-autorenew"></i>
-                                        </a>
-                                        <a href="javascript: void(0);" class="btn btn-primary ms-1">
-                                            <i class="mdi mdi-filter-variant"></i>
-                                        </a>
                                     </form>
                                 </div>
                                 <h4 class="page-title">Indicadores</h4>
