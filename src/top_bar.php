7b028cf1116afa0526561f53496a1e7a680a87fa
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 17:32:44 +0000

    Deploy

--- src/top_bar.php
+++ src/top_bar.php
@@ -19,10 +19,10 @@
 
 	$blocoSession = $_SESSION['user_bloco'];
 	$apartamentoSession = $_SESSION['user_apartamento'];
-	$nomeSession =  ucfirst($_SESSION['user_name']); 
+	$nomeSession =  ucfirst(strtolower($_SESSION['user_name'])); 
     $nomeSessionShort =  substr($nomeSession,0,15);
 	$usuariologado = $nomeSession." <b>BL</b> ".$blocoSession." <b>AP</b> ".$apartamentoSession;
-    $nivelAcesso = ucfirst($_SESSION['user_nivelacesso']);
+    $nivelAcesso = ucfirst(strtolower($_SESSION['user_nivelacesso']));
 	$userid = $_SESSION['user_id'];
 ?>
 
