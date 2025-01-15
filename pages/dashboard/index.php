47e659a349c0822bc1ff142856ff87039508573b
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 22:23:10 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -285,7 +285,8 @@
                         </div> <!-- end col-->
 
                         <div class="col-lg-4">
-                        <div class="d-flex card-header justify-content-between align-items-center">
+                        <div class="card">
+                                <div class="d-flex card-header justify-content-between align-items-center">
                                     <h4 class="header-title">Total Sales</h4>
                                     <div class="dropdown">
                                         <a href="#" class="dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown" aria-expanded="false">
@@ -303,6 +304,31 @@
                                         </div>
                                     </div>
                                 </div>
+
+                                <div class="card-body pt-0">
+                                    <div id="average-sales" class="apex-charts mb-4 mt-2" data-colors="#727cf5,#0acf97,#fa5c7c,#ffbc00"></div>
+
+
+                                    <div class="chart-widget-list">
+                                        <p>
+                                            <i class="mdi mdi-square text-primary"></i> Direct
+                                            <span class="float-end">$300.56</span>
+                                        </p>
+                                        <p>
+                                            <i class="mdi mdi-square text-danger"></i> Affilliate
+                                            <span class="float-end">$135.18</span>
+                                        </p>
+                                        <p>
+                                            <i class="mdi mdi-square text-success"></i> Sponsored
+                                            <span class="float-end">$48.96</span>
+                                        </p>
+                                        <p class="mb-0">
+                                            <i class="mdi mdi-square text-warning"></i> E-mail
+                                            <span class="float-end">$154.02</span>
+                                        </p>
+                                    </div>
+                                </div> <!-- end card-body-->
+                            </div> <!-- end card-->
                         </div> <!-- end col-->
                     </div>
                     <!-- end row -->
