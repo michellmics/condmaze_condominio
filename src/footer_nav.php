bf417f69db471fbd51c5d3db3b350361b6918684
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 10:59:42 +0000

    Deploy

--- src/footer_nav.php
+++ src/footer_nav.php
@@ -9,7 +9,61 @@
     if(isset($siteAdmin->ARRAY_FOOTERPUBLISHINFO[$num]["PUB_DCDESC"])){$publiText = $siteAdmin->ARRAY_FOOTERPUBLISHINFO[$num]["PUB_DCDESC"];}
 ?>
 
+<style>
+  /* Estilo para o container do texto rolante */
+  .marquee-container {
+    display: block;
+    white-space: nowrap;
+    overflow: hidden;
+    width: 100%; /* Ajusta a largura conforme necessário */
+    box-sizing: border-box;
+    background-color:rgb(21, 129, 179); /* Exemplo de cor de fundo para o marquee */
+    padding: 3px 0; /* Espaçamento para o marquee */
+  }
+
+  .marquee-container span {
+    display: inline-block;
+    animation: marquee 20s linear infinite;
+    white-space: nowrap; /* Garante que o texto não quebre a linha */
+    color: red;
+  }
+
+  /* Animação para rolar o texto */
+  @keyframes marquee {
+    0% {
+      transform: translateX(100%); /* Começa fora da tela à direita */
+    }
+    100% {
+      transform: translateX(-100%); /* Termina fora da tela à esquerda */
+    }
+  }
+
+  /* Estilo do conteúdo abaixo do marquee */
+  .footer-info {
+    text-align: center;
+    font-size: 11px;
+    color: #333;
+  }
+
+  .footer-info a {
+    color: #007bff;
+    text-decoration: none;
+  }
+
+  .marquee-container p {
+  color:rgb(243, 243, 243); /* Cor do texto do <p> */
+}
+
+  .footer-info a:hover {
+    text-decoration: underline;
+  }
+</style>
+
 <footer class="footer">
+      <!-- Marquee com o conteúdo rolante -->
+  <div class="marquee-container">
+    <span><?php echo "texxxx"; ?></span>
+  </div>
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
