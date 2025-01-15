93343cf70a3c74333b630596419ec3891ebdc5b4
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 22:35:34 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -82,20 +82,24 @@
                         <div class="col-12">
                             <div class="page-title-box">
                                 <div class="page-title-right">
-                                    <form class="d-flex">
-                                        <div class="input-group">
-                                            <input type="text" class="form-control form-control-light" id="dash-daterange">
-                                            <span class="input-group-text bg-primary border-primary text-white">
-                                                <i class="mdi mdi-calendar-range font-13"></i>
-                                            </span>
-                                        </div>
-                                        <a href="javascript: void(0);" class="btn btn-primary ms-2">
-                                            <i class="mdi mdi-autorenew"></i>
-                                        </a>
-                                        <a href="javascript: void(0);" class="btn btn-primary ms-1">
-                                            <i class="mdi mdi-filter-variant"></i>
-                                        </a>
-                                    </form>
+                                <form class="d-flex">
+    <div class="input-group">
+        <select class="form-control form-control-light" id="dash-daterange">
+            <option value="opcao1">Opção 1</option>
+            <option value="opcao2">Opção 2</option>
+            <option value="opcao3">Opção 3</option>
+        </select>
+        <span class="input-group-text bg-primary border-primary text-white">
+            <i class="mdi mdi-calendar-range font-13"></i>
+        </span>
+    </div>
+    <a href="javascript: void(0);" class="btn btn-primary ms-2">
+        <i class="mdi mdi-autorenew"></i>
+    </a>
+    <a href="javascript: void(0);" class="btn btn-primary ms-1">
+        <i class="mdi mdi-filter-variant"></i>
+    </a>
+</form>
                                 </div>
                                 <h4 class="page-title">Indicadores</h4>
                             </div>
