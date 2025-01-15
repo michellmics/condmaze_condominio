14113360ab63a51ade3c6939c361fc87253f26c6
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-15 11:03:04 +0000

    Deploy

--- src/footer_nav.php
+++ src/footer_nav.php
@@ -58,9 +58,7 @@
     text-decoration: underline;
   }
 </style>
-<div class="marquee-container">
-    <span><?php echo "texxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxx"; ?></span>
-  </div>
+
 <footer class="footer">
     <div class="container-fluid">
         <div class="row">
@@ -75,15 +73,9 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6">               
-                <a href="https://codemaze.com.br" target="_blank"><b>Codemaze</b></a> - Soluções de Mkt e Software | 
-                <b><font color="red"><?php echo $_SESSION['user_nivelacesso']; ?></font></b> | 
-            </div>
-            <div class="col-md-6">
-                <div class="text-md-end footer-links d-none d-md-block">
-                    <a href="javascript: void(0);">Webmail</a>
-                </div>
-            </div>
+        <div class="marquee-container">
+    <span><?php echo "texxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxxexxxx"; ?></span>
+  </div>
         </div>
     </div>
 </footer>
