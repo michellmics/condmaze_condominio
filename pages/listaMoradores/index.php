a2261e4ebd045842709e77278db21ff8258d45cc
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-16 22:33:39 +0000

    Deploy

--- pages/listaMoradores/index.php
+++ pages/listaMoradores/index.php
@@ -122,10 +122,10 @@
                                                 <tbody>
                                                     <?php foreach ($siteAdmin->ARRAY_LISTAMORADORESINFO as $item): ?>
                                                         <tr>
-                                                            <td style="font-size: 12px;"><?= htmlspecialchars($item['USU_DCNOME'])."..."; ?></td>
-                                                            <td style="font-size: 12px;"><?= htmlspecialchars($item['USU_DCBLOCO']); ?></td>
-                                                            <td style="font-size: 12px;"><?= htmlspecialchars($item['USU_DCAPARTAMENTO']); ?></td>
-                                                            <td style="font-size: 12px;"><?= htmlspecialchars($item['USU_DCEMAIL']); ?></td>
+                                                            <td><?= htmlspecialchars($item['USU_DCNOME']); ?></td>
+                                                            <td><?= htmlspecialchars($item['USU_DCBLOCO']); ?></td>
+                                                            <td><?= htmlspecialchars($item['USU_DCAPARTAMENTO']); ?></td>
+                                                            <td><?= htmlspecialchars($item['USU_DCEMAIL']); ?></td>
                                                             <td><i data-lucide="edit" style="color: #6699ff;"></i></td>
                                                             <td><i class="ri-close-circle-line" style="color: #ed5628;"></i></td>
                                                         </tr>
