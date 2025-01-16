15e7dac144bb002dd7163500a8e575b49b2336f5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-16 20:14:40 +0000

    Deploy

--- pages/listaMoradores/index.php
+++ pages/listaMoradores/index.php
@@ -95,15 +95,12 @@
                     <!-- end page title -->
 
                     <div class="row">
-                        <div class="col-lg-12">
+                        <div class="col-lg-6">
                             <div class="card">
                                 <div class="card-body">
-                                    <h4 class="header-title">Basic Data Table</h4>
-                                    <p class="text-muted font-14">
-                                        DataTables has most features enabled by default, so all you need to do to use it with your own tables is to call the construction
-                                        function:
-                                        <code>$().DataTable();</code>. KeyTable provides Excel like cell navigation on any table. Events (focus, blur, action etc) can be assigned to individual
-                                        cells, columns, rows or all cells.
+                                    <h4 class="header-title">Cadastro de Convidado</h4>
+                                    <p class="text-muted font-14">Cadastre seu convidado aqui e lembre-se de ativá-lo para 
+                                        garantir o acesso ao condomínio durante o uso do salão de festas.
                                     </p>
                                     <div class="tab-content">
                                         <div class="tab-pane show active" id="basic-datatable-preview">
