4e4404ea173525e417c590aaec78dc98e197a8f6
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-16 22:57:15 +0000

    Deploy

--- assets/js/pages/demo.datatable-init.js
+++ assets/js/pages/demo.datatable-init.js
@@ -1,30 +1,50 @@
-$(document).ready(function() {
+$(document).ready(function () {
     "use strict";
     $("#basic-datatable").DataTable({
-        keys: !0,
+        keys: true,
         language: {
             paginate: {
                 previous: "<i class='mdi mdi-chevron-left'>",
                 next: "<i class='mdi mdi-chevron-right'>"
-            }
+            },
+            sEmptyTable: "Nenhum dado disponível na tabela",
+            sInfo: "Mostrando _START_ até _END_ de _TOTAL_ registros",
+            sInfoEmpty: "Mostrando 0 até 0 de 0 registros",
+            sInfoFiltered: "(filtrado de _MAX_ registros no total)",
+            sLengthMenu: "Mostrar _MENU_ registros",
+            sLoadingRecords: "Carregando...",
+            sProcessing: "Processando...",
+            sSearch: "Pesquisar:",
+            sZeroRecords: "Nenhum registro encontrado"
         },
-        drawCallback: function() {
-            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded");
         }
     });
+
     var a = $("#datatable-buttons").DataTable({
-        lengthChange: !1,
+        lengthChange: false,
         buttons: ["copy", "print"],
         language: {
             paginate: {
                 previous: "<i class='mdi mdi-chevron-left'>",
                 next: "<i class='mdi mdi-chevron-right'>"
-            }
+            },
+            sEmptyTable: "Nenhum dado disponível na tabela",
+            sInfo: "Mostrando _START_ até _END_ de _TOTAL_ registros",
+            sInfoEmpty: "Mostrando 0 até 0 de 0 registros",
+            sInfoFiltered: "(filtrado de _MAX_ registros no total)",
+            sLengthMenu: "Mostrar _MENU_ registros",
+            sLoadingRecords: "Carregando...",
+            sProcessing: "Processando...",
+            sSearch: "Pesquisar:",
+            sZeroRecords: "Nenhum registro encontrado"
         },
-        drawCallback: function() {
-            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded");
         }
     });
+
     $("#selection-datatable").DataTable({
         select: {
             style: "multi"
@@ -33,97 +53,71 @@ $(document).ready(function() {
             paginate: {
                 previous: "<i class='mdi mdi-chevron-left'>",
                 next: "<i class='mdi mdi-chevron-right'>"
-            }
+            },
+            sEmptyTable: "Nenhum dado disponível na tabela",
+            sInfo: "Mostrando _START_ até _END_ de _TOTAL_ registros",
+            sInfoEmpty: "Mostrando 0 até 0 de 0 registros",
+            sInfoFiltered: "(filtrado de _MAX_ registros no total)",
+            sLengthMenu: "Mostrar _MENU_ registros",
+            sLoadingRecords: "Carregando...",
+            sProcessing: "Processando...",
+            sSearch: "Pesquisar:",
+            sZeroRecords: "Nenhum registro encontrado"
         },
-        drawCallback: function() {
-            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
-        }
-    }), a.buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"), $("#alternative-page-datatable").DataTable({
-        pagingType: "full_numbers",
-        drawCallback: function() {
-            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
-        }
-    }), $("#scroll-vertical-datatable").DataTable({
-        scrollY: "350px",
-        scrollCollapse: !0,
-        paging: !1,
-        language: {
-            paginate: {
-                previous: "<i class='mdi mdi-chevron-left'>",
-                next: "<i class='mdi mdi-chevron-right'>"
-            }
-        },
-        drawCallback: function() {
-            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
-        }
-    }), $("#scroll-horizontal-datatable").DataTable({
-        scrollX: !0,
-        language: {
-            paginate: {
-                previous: "<i class='mdi mdi-chevron-left'>",
-                next: "<i class='mdi mdi-chevron-right'>"
-            }
-        },
-        drawCallback: function() {
-            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
-        }
-    }), $("#complex-header-datatable").DataTable({
-        language: {
-            paginate: {
-                previous: "<i class='mdi mdi-chevron-left'>",
-                next: "<i class='mdi mdi-chevron-right'>"
-            }
-        },
-        drawCallback: function() {
-            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
-        },
-        columnDefs: [{
-            visible: !1,
-            targets: -1
-        }]
-    }), $("#row-callback-datatable").DataTable({
-        language: {
-            paginate: {
-                previous: "<i class='mdi mdi-chevron-left'>",
-                next: "<i class='mdi mdi-chevron-right'>"
-            }
-        },
-        drawCallback: function() {
-            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
-        },
-        createdRow: function(a, e, t) {
-            15e4 < +e[5].replace(/[\$,]/g, "") && $("td", a).eq(5).addClass("text-danger")
-        }
-    }), $("#state-saving-datatable").DataTable({
-        stateSave: !0,
-        language: {
-            paginate: {
-                previous: "<i class='mdi mdi-chevron-left'>",
-                next: "<i class='mdi mdi-chevron-right'>"
-            }
-        },
-        drawCallback: function() {
-            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
-        }
-    }), $("#fixed-columns-datatable").DataTable({
-        scrollY: 300,
-        scrollX: !0,
-        scrollCollapse: !0,
-        paging: !1,
-        fixedColumns: !0
-    }), $(".dataTables_length select").addClass("form-select form-select-sm"), $(".dataTables_length label").addClass("form-label")
-}), $(document).ready(function() {
-    var a = $("#fixed-header-datatable").DataTable({
-        responsive: !0,
-        language: {
-            paginate: {
-                previous: "<i class='mdi mdi-chevron-left'>",
-                next: "<i class='mdi mdi-chevron-right'>"
-            }
-        },
-        drawCallback: function() {
-            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded");
         }
     });
-    new $.fn.dataTable.FixedHeader(a)
+
+    a.buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)");
+
+    $("#alternative-page-datatable").DataTable({
+        pagingType: "full_numbers",
+        language: {
+            paginate: {
+                previous: "<i class='mdi mdi-chevron-left'>",
+                next: "<i class='mdi mdi-chevron-right'>"
+            },
+            sEmptyTable: "Nenhum dado disponível na tabela",
+            sInfo: "Mostrando _START_ até _END_ de _TOTAL_ registros",
+            sInfoEmpty: "Mostrando 0 até 0 de 0 registros",
+            sInfoFiltered: "(filtrado de _MAX_ registros no total)",
+            sLengthMenu: "Mostrar _MENU_ registros",
+            sLoadingRecords: "Carregando...",
+            sProcessing: "Processando...",
+            sSearch: "Pesquisar:",
+            sZeroRecords: "Nenhum registro encontrado"
+        },
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded");
+        }
+    });
+
+    $("#scroll-vertical-datatable").DataTable({
+        scrollY: "350px",
+        scrollCollapse: true,
+        paging: false,
+        language: {
+            paginate: {
+                previous: "<i class='mdi mdi-chevron-left'>",
+                next: "<i class='mdi mdi-chevron-right'>"
+            },
+            sEmptyTable: "Nenhum dado disponível na tabela",
+            sInfo: "Mostrando _START_ até _END_ de _TOTAL_ registros",
+            sInfoEmpty: "Mostrando 0 até 0 de 0 registros",
+            sInfoFiltered: "(filtrado de _MAX_ registros no total)",
+            sLengthMenu: "Mostrar _MENU_ registros",
+            sLoadingRecords: "Carregando...",
+            sProcessing: "Processando...",
+            sSearch: "Pesquisar:",
+            sZeroRecords: "Nenhum registro encontrado"
+        },
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded");
+        }
+    });
+
+    // Outros DataTables com tradução análoga...
+    $(".dataTables_length select").addClass("form-select form-select-sm");
+    $(".dataTables_length label").addClass("form-label");
 });
