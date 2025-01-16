82ac9a1454216ea8b3bad4adc198635fb7c1b818
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-16 18:53:31 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -302,7 +302,7 @@
                     <!-- end row -->
 
                     <div class="row">
-                        <div class="col-lg-8">
+                        <div class="col-lg-12">
                         <div class="card">
                             <div class="d-flex card-header justify-content-between align-items-center">
                                 <h4 class="header-title">Top 10 Despesas</h4>
@@ -328,54 +328,6 @@
                                             </tbody>
                                         </table>
                                     </div> <!-- end table-responsive-->
-                                </div> <!-- end card-body-->
-                            </div> <!-- end card-->
-                        </div> <!-- end col-->
-
-                        <div class="col-lg-4">
-                            <div class="card">
-                                <div class="d-flex card-header justify-content-between align-items-center">
-                                    <h4 class="header-title">Despesas</h4>
-                                    <div class="dropdown">
-                                        <a href="#" class="dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown" aria-expanded="false">
-                                            <i class="mdi mdi-dots-vertical"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-end">
-                                            <!-- item-->
-                                            <a href="javascript:void(0);" class="dropdown-item">Sales Report</a>
-                                            <!-- item-->
-                                            <a href="javascript:void(0);" class="dropdown-item">Export Report</a>
-                                            <!-- item-->
-                                            <a href="javascript:void(0);" class="dropdown-item">Profit</a>
-                                            <!-- item-->
-                                            <a href="javascript:void(0);" class="dropdown-item">Action</a>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card-body pt-0">
-                                    <div id="average-sales" class="apex-charts mb-4 mt-2" data-colors="#727cf5,#0acf97,#fa5c7c,#ffbc00"></div>
-
-
-                                    <div class="chart-widget-list">
-                                        <p>
-                                            <i class="mdi mdi-square text-primary"></i> Direct
-                                            <span class="float-end">$300.56</span>
-                                        </p>
-                                        <p>
-                                            <i class="mdi mdi-square text-danger"></i> Affilliate
-                                            <span class="float-end">$135.18</span>
-                                        </p>
-                                        <p>
-                                            <i class="mdi mdi-square text-success"></i> Sponsored
-                                            <span class="float-end">$48.96</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <i class="mdi mdi-square text-warning"></i> E-mail
-                                            <span class="float-end">$154.02</span>
-                                        </p>
-                                        
-                                    </div>
                                 </div> <!-- end card-body-->
                             </div> <!-- end card-->
                         </div> <!-- end col-->
