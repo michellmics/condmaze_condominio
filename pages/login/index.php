8417fdbd0938d32b9f31d4006eda695c01beb4f0
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-16 03:01:51 +0000

    Deploy

--- pages/login/index.php
+++ pages/login/index.php
@@ -12,16 +12,16 @@
     <link rel="shortcut icon" href="assets/images/favicon.ico">
 
     <!-- Theme Config Js -->
-    <script src="assets/js/hyper-config.js"></script>
+    <script src="../../assets/js/hyper-config.js"></script>
 
     <!-- Vendor css -->
-    <link href="assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
 
     <!-- App css -->
-    <link href="assets/css/app-modern.min.css" rel="stylesheet" type="text/css" id="app-style" />
+    <link href="../../assets/css/app-modern.min.css" rel="stylesheet" type="text/css" id="app-style" />
 
     <!-- Icons css -->
-    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body class="authentication-bg pb-0">
@@ -34,10 +34,10 @@
                 <!-- Logo -->
                 <div class="auth-brand text-center text-lg-start">
                     <a href="index.html" class="logo-dark">
-                        <span><img src="assets/images/logo-dark.png" alt="dark logo" height="22"></span>
+                        <span><img src="../../assets/images/logo-dark.png" alt="dark logo" height="22"></span>
                     </a>
                     <a href="index.html" class="logo-light">
-                        <span><img src="assets/images/logo.png" alt="logo" height="22"></span>
+                        <span><img src="../../assets/images/logo.png" alt="logo" height="22"></span>
                     </a>
                 </div>
 
@@ -112,10 +112,10 @@
     </div>
     <!-- end auth-fluid-->
     <!-- Vendor js -->
-    <script src="assets/js/vendor.min.js"></script>
+    <script src="../../assets/js/vendor.min.js"></script>
 
     <!-- App js -->
-    <script src="assets/js/app.min.js"></script>
+    <script src="../../assets/js/app.min.js"></script>
 
 </body>
 
