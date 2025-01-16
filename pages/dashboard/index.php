7cbfc2a8dd6ade0118aec9e012de1f33fc8803d8
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-16 18:14:06 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -315,12 +315,8 @@
                                     <table id="basic-datatable" class="table dt-responsive nowrap w-100">
                                         <thead>
                                             <tr>
-                                                <th>Name</th>
-                                                <th>Position</th>
-                                                <th>Office</th>
-                                                <th>Age</th>
-                                                <th>Start date</th>
-                                                <th>Salary</th>
+                                                <th>Descrição / Fornecedor</th>
+                                                <th>Valor</th>
                                             </tr>
                                         </thead>
 
@@ -328,18 +324,10 @@
                                         <tbody>
                                             <tr>
                                                 <td>Tiger Nixon</td>
-                                                <td>System Architect</td>
-                                                <td>Edinburgh</td>
-                                                <td>61</td>
-                                                <td>2011/04/25</td>
                                                 <td>$320,800</td>
                                             </tr>
                                             <tr>
                                                 <td>Garrett Winters</td>
-                                                <td>Accountant</td>
-                                                <td>Tokyo</td>
-                                                <td>63</td>
-                                                <td>2011/07/25</td>
                                                 <td>$170,750</td>
                                             </tr>
                                         </tbody>
