b025f7d4600e11b373637ca466df7c32d4ba38f5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-16 22:12:38 +0000

    Deploy

--- pages/listaMoradores/index.php
+++ pages/listaMoradores/index.php
@@ -119,7 +119,7 @@
                                                 <tbody>
                                                     <?php foreach ($siteAdmin->ARRAY_LISTAMORADORESINFO as $item): ?>
                                                         <tr>
-                                                            <td><?= htmlspecialchars(substr($item['USU_DCNOME'],0,15)); ?></td>
+                                                            <td><?= htmlspecialchars(substr($item['USU_DCNOME'],0,15))."..."; ?></td>
                                                             <td><?= htmlspecialchars($item['USU_DCAPARTAMENTO']); ?></td>
                                                             <td><i data-lucide="edit" style="color: #6699ff;"></i></td>
                                                             <td><i class="ri-close-circle-line" style="color: #ed5628;"></i></td>
