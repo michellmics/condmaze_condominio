97c5558d04be17ad8cffe04787701c383f7b495b
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-16 20:10:28 +0000

    Deploy

--- pages/listaMoradores/index.php
+++ pages/listaMoradores/index.php
@@ -86,13 +86,8 @@
                         <div class="col-12">
                             <div class="page-title-box">
                                 <div class="page-title-right">
-                                    <ol class="breadcrumb m-0">
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
-                                        <li class="breadcrumb-item active">Data Tables</li>
-                                    </ol>
                                 </div>
-                                <h4 class="page-title">Data Tables</h4>
+                                <h4 class="page-title">Lista de Convidados</h4>
                             </div>
                         </div>
                     </div>
