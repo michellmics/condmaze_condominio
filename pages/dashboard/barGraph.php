6d4dd622a720c88e5bf0d497dbf3c90b6de5d760
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-16 01:23:47 +0000

    Deploy

---
+++ pages/dashboard/barGraph.php
@@ -0,0 +1,14 @@
+<?php
+    ini_set('display_errors', 1);  // Habilita a exibição de erros
+    error_reporting(E_ALL);        // Reporta todos os erros
+    header('Content-Type: application/json');
+    
+	include_once '../../objects/objects_chart.php'; 
+
+   $chartValor->getPendenciaByMesFull();
+   echo json_encode($chartValor->ARRAY_PENDENCIAMESFULLINFO);
+
+    
+
+    
+?>
