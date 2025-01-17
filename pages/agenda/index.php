41c050583e8f084c0db31b53a7af06c724d5d65f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-17 01:59:09 +0000

    Deploy

--- pages/agenda/index.php
+++ pages/agenda/index.php
@@ -53,13 +53,8 @@
                         <div class="col-12">
                             <div class="page-title-box">
                                 <div class="page-title-right">
-                                    <ol class="breadcrumb m-0">
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Apps</a></li>
-                                        <li class="breadcrumb-item active">Calendar</li>
-                                    </ol>
                                 </div>
-                                <h4 class="page-title">Calendar</h4>
+                                <h4 class="page-title">Agenda</h4>
                             </div>
                         </div>
                     </div>
