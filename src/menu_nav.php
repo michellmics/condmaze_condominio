b5516ba4a4804664b3c404a1661bda02d9ab83a5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-20 01:54:51 +0000

    Deploy

--- src/menu_nav.php
+++ src/menu_nav.php
@@ -7,7 +7,7 @@
                     <div class="collapse navbar-collapse" id="topnav-menu-content">
                         <ul class="navbar-nav">
                             <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-components" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle arrow-none" href="../inicial/index.php" id="topnav-components" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <i class="uil-home-alt"></i>Início <div class="dropdown-item"></div>
                                 </a>
                             </li>
