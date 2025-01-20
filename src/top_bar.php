6cb48de31659541122f95dd2058a47854448a289
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-20 03:07:56 +0000

    Deploy

--- src/top_bar.php
+++ src/top_bar.php
@@ -39,7 +39,7 @@
                     <!-- Topbar Brand Logo -->
                     <div class="logo-topbar">
                         <!-- Logo light -->
-                        <a href="index.html" class="logo-light">
+                        <a href="../pages/inicial/index.html" class="logo-light">
                             <span class="logo-lg">
                                 <img src="../../img/logo_128x32_white.png" alt="logo">
                             </span>
@@ -49,7 +49,7 @@
                         </a>
 
                         <!-- Logo Dark -->
-                        <a href="index.html" class="logo-dark">
+                        <a href="../pages/inicial/index.html" class="logo-dark">
                             <span class="logo-lg">
                                 <img src="../../img/logo_128x32_black.png" alt="dark logo">
                             </span>
