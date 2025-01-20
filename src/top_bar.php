7485b712b9707b866eda59f1cd13910d862efb6f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-20 04:10:22 +0000

    Deploy

--- src/top_bar.php
+++ src/top_bar.php
@@ -80,12 +80,8 @@
                 </div>
 
                 <ul class="topbar-menu d-flex align-items-center gap-3">
-                dddddddddddd
-                    <li class="d-none d-sm-inline-block">
-                        <div class="nav-link" id="light-dark-mode" data-bs-toggle="tooltip" data-bs-placement="left" title="Alternar brilho">
-                            <i class="ri-moon-line font-22"></i>
-                        </div>
-                    </li>
+                gg
+
 
                     
 <!--
