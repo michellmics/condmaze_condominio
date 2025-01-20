cb43e3494f4343b54795a6c2a99f1717d9a20373
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-20 02:57:14 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -66,7 +66,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title><?php echo $nomeCondominio; ?></title>
+    <title><?php //echo $nomeCondominio; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
     <meta content="Coderthemes" name="author" />
