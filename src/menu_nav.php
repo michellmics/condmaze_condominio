9fed58a7d3a410227abf0331ebe2c9381656c32f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-20 02:04:09 +0000

    Deploy

--- src/menu_nav.php
+++ src/menu_nav.php
@@ -7,8 +7,8 @@
                     <div class="collapse navbar-collapse" id="topnav-menu-content">
                         <ul class="navbar-nav">
                             <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle arrow-none" href="../listaMoradores/index.php" id="topnav-components" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="uil-home-alt"></i><a href="../listaMoradores/index.php">Início </a><div class="dropdown-item"></div>
+                            <a class="nav-link dropdown-toggle arrow-none" href="../listaMoradores/index.php" id="topnav-components" role="button" aria-haspopup="true" aria-expanded="false">
+                                    <i class="uil-home-alt"></i>Início <div class="dropdown-item"></div>
                                 </a>
                             </li>
                             <li class="nav-item dropdown">
