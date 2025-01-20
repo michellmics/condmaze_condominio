c5c8c07866370eb949df55c25b43227c2c42577d
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-20 02:43:48 +0000

    Deploy

--- pages/inicial/index.php
+++ pages/inicial/index.php
@@ -5,7 +5,7 @@
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
     $host = $_SERVER['HTTP_HOST'];
     $baseUrl = $protocol . "://" . $host;
-    $webmailUrl = $baseUrl . "//api//";
+    $webmailUrl = $baseUrl . "/api//";
 
 	include_once "../../objects/objects.php";
 	
