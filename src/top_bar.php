f841178af6f691220fd845964c4e0ec2613df7e5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-20 04:21:34 +0000

    Deploy

--- src/top_bar.php
+++ src/top_bar.php
@@ -81,7 +81,7 @@
                 <ul class="topbar-menu d-flex align-items-center gap-3">
     <?php echo $nomeSession . " - BL: " . $blocoSession . " AP: " . $apartamentoSession; ?>
     <br>
-    <span style="font-size: 8; color: red;"><?php echo $nivelAcesso; ?></span>
+    <span style="font-size: 6; color: red;"><?php echo $nivelAcesso; ?></span>
 </ul>
 
 
