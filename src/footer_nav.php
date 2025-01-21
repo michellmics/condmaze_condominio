655b143534a70bb083ce935ab0efb1ffc12424a5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-21 02:31:34 +0000

    Deploy

--- src/footer_nav.php
+++ src/footer_nav.php
@@ -28,6 +28,9 @@
             <div class="col-md-6">               
                 <a href="https://codemaze.com.br" target="_blank"><b>Codemaze</b></a> - Soluções de Mkt e Software     
             </div>
+            <div class="col-md-6">               
+                <a href="https://codemaze.com.br" target="_blank"><b>Codemaze</b></a> - Soluções de Mkt e Software     
+            </div>
         </div>
     </div>
 </footer>
