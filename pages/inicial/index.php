0c0269fe2b2f5b0221d50fec9779377f52f3e2e9
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-23 18:17:54 +0000

    Deploy

--- pages/inicial/index.php
+++ pages/inicial/index.php
@@ -314,6 +314,8 @@
                                                                 vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
                                                                 in faucibus.
                                                                 <BR>
+                                                                <BR>
+                                                                <BR>
                                                                 <h5 class="mt-0">22/01/2025 13:00:12</h5>
                                                                 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
                                                                 vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
