9d77e8b7665b65c4f1a7c68ff7e36a7cdf357e42
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-23 16:23:41 +0000

    Deploy

--- src/menu_nav.php
+++ src/menu_nav.php
@@ -36,7 +36,7 @@
                             </li>    
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-components" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="uil uil-tachometer-fast"></i>Portaria <div class="arrow-down"></div>
+                                    <i class="uil uil-building"></i>Portaria <div class="arrow-down"></div>
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="topnav-components">
                                     <a href="../encomendas/index.php" class="dropdown-item">Encomendas</a>
