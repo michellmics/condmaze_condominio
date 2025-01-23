ee3cedcf0060d23ca271d9e403f410408c57b2f5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-23 18:19:21 +0000

    Deploy

--- pages/inicial/index.php
+++ pages/inicial/index.php
@@ -311,15 +311,37 @@
                                                             <div class="w-100 overflow-hidden">
                                                                 <h5 class="mt-0">22/01/2025 13:00:12</h5>
                                                                 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-                                                                vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                                                                in faucibus.
+                                                                vulputate at  tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
                                                                 <BR>
                                                                 <BR>
                                                                 <BR>
                                                                 <h5 class="mt-0">22/01/2025 13:00:12</h5>
                                                                 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-                                                                vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
-                                                                in faucibus.
+                                                                vulputate at  tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                                                <BR>
+                                                                <BR>
+                                                                <BR>
+                                                                <h5 class="mt-0">22/01/2025 13:00:12</h5>
+                                                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                                                vulputate at  tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                                                <BR>
+                                                                <BR>
+                                                                <BR>
+                                                                <h5 class="mt-0">22/01/2025 13:00:12</h5>
+                                                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                                                vulputate at  tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                                                <BR>
+                                                                <BR>
+                                                                <BR>
+                                                                <h5 class="mt-0">22/01/2025 13:00:12</h5>
+                                                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                                                vulputate at  tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                                                <BR>
+                                                                <BR>
+                                                                <BR>
+                                                                <h5 class="mt-0">22/01/2025 13:00:12</h5>
+                                                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                                                vulputate at  tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
                                                             </div>
 
                                                             
