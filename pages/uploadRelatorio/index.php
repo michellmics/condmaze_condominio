d238badd0fe3def32b7f988ff63116823642117d
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-23 04:13:47 +0000

    Deploy

--- pages/uploadRelatorio/index.php
+++ pages/uploadRelatorio/index.php
@@ -112,6 +112,31 @@
                                                     </div>
                                                 </div>
 
+                                                <div class="form-row" style="margin-bottom: 10px;  margin: 10px;">
+                                                    <div>
+                                                        <select class="form-control" id="ano" name="ano" required>
+                                                            <option value="" disabled selected>Ano de Referência</option>
+                                                            <option value="2024">2024</option>
+                                                            <option value="2025">2025</option>
+                                                            <option value="2026">2026</option>
+                                                            <option value="2027">2027</option>
+                                                            <option value="2028">2028</option>
+                                                            <option value="2029">2029</option>
+                                                            <option value="2030">2030</option>
+                                                            <option value="2031">2031</option>
+                                                            <option value="2032">2032</option>
+                                                            <option value="2033">2033</option>
+                                                            <option value="2034">2034</option>
+                                                            <option value="2035">2035</option>
+                                                            <option value="2036">2036</option>
+                                                            <option value="2037">2037</option>
+                                                            <option value="2038">2038</option>
+                                                            <option value="2039">2039</option>
+                                                            <option value="2040">2040</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
                                                 <h6 class="font-15 mt-3">Tipo de dados</h6>
                                                 <div class="mt-2">
                                                     <div class="form-check form-check-inline">
