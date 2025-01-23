bce672d85942da92b0eac9bfd66c7f9b74c9ecee
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-23 17:28:08 +0000

    Deploy

--- pages/inicial/index.php
+++ pages/inicial/index.php
@@ -191,58 +191,22 @@
                                                 <table class="table table-centered mb-0">
                                                     <thead>
                                                         <tr>
-                                                            <th>Name</th>
-                                                            <th>Phone Number</th>
-                                                            <th>Date of Birth</th>
-                                                            <th>Active?</th>
+                                                            <th>ID</th>
+                                                            <th>NOME</th>
+                                                            <th>DATA REGISTRO</th>
+                                                            <th>RECEBER?</th>
                                                         </tr>
                                                     </thead>
                                                     <tbody>
                                                         <tr>
-                                                            <td>Risa D. Pearson</td>
-                                                            <td>336-508-2157</td>
-                                                            <td>July 24, 1950</td>
+                                                            <td>12569</td>
+                                                            <td>MICHELL DUARTE DE OLIVEIRA</td>
+                                                            <td>12/08/2025 12:32</td>
                                                             <td>
                                                                 <!-- Switch-->
                                                                 <div>
                                                                     <input type="checkbox" id="switch01" checked data-switch="success" />
-                                                                    <label for="switch01" data-on-label="Yes" data-off-label="No" class="mb-0 d-block"></label>
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Ann C. Thompson</td>
-                                                            <td>646-473-2057</td>
-                                                            <td>January 25, 1959</td>
-                                                            <td>
-                                                                <!-- Switch-->
-                                                                <div>
-                                                                    <input type="checkbox" id="switch02" checked data-switch="success" />
-                                                                    <label for="switch02" data-on-label="Yes" data-off-label="No" class="mb-0 d-block"></label>
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Paul J. Friend</td>
-                                                            <td>281-308-0793</td>
-                                                            <td>September 1, 1939</td>
-                                                            <td>
-                                                                <!-- Switch-->
-                                                                <div>
-                                                                    <input type="checkbox" id="switch03" data-switch="success" />
-                                                                    <label for="switch03" data-on-label="Yes" data-off-label="No" class="mb-0 d-block"></label>
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Linda G. Smith</td>
-                                                            <td>606-253-1207</td>
-                                                            <td>May 3, 1962</td>
-                                                            <td>
-                                                                <!-- Switch-->
-                                                                <div>
-                                                                    <input type="checkbox" id="switch04" data-switch="success" />
-                                                                    <label for="switch04" data-on-label="Yes" data-off-label="No" class="mb-0 d-block"></label>
+                                                                    <label for="switch01" data-on-label="Sim" data-off-label="Não" class="mb-0 d-block"></label>
                                                                 </div>
                                                             </td>
                                                         </tr>
