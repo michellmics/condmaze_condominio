1786b36781b51de4d262c54b8b2985ee16358fd5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-24 17:28:59 +0000

    Deploy

--- pages/logoff/index.php
+++ pages/logoff/index.php
@@ -2,5 +2,6 @@
     session_start();
     $_SESSION = [];
     session_destroy();
+    echo "<script>    localStorage.removeItem('authToken');    </script>"
     header("Location: ../login/index.php");
 ?>
