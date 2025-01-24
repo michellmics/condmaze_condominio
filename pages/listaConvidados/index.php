3926788a9b1597ef134d02f87855cc31a5f61a52
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-24 14:04:50 +0000

    Deploy

--- pages/listaConvidados/index.php
+++ pages/listaConvidados/index.php
@@ -25,9 +25,6 @@
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
     <meta content="Coderthemes" name="author" />
 
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="../../assets/images/favicon.ico">
-
     <!-- Datatables css -->
     <link href="../../assets/vendor/datatables.net-bs5/css/dataTables.bootstrap5.min.css" rel="stylesheet" type="text/css" />
     <link href="../../assets/vendor/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css" rel="stylesheet" type="text/css" />
