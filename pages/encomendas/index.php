108b17fec0b90ff04353cab137be3d8fa57d8dec
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-24 03:23:48 +0000

    Deploy

--- pages/encomendas/index.php
+++ pages/encomendas/index.php
@@ -256,29 +256,17 @@
                 <form class="ps-3 pe-3" action="#">
 
                     <div class="mb-3">
-                        <label for="username" class="form-label">Name</label>
+                        <label for="username" class="form-label">Apartamento</label>
                         <input class="form-control" type="email" id="username" required="" placeholder="Michael Zenaty">
                     </div>
 
                     <div class="mb-3">
-                        <label for="emailaddress" class="form-label">Email address</label>
-                        <input class="form-control" type="email" id="emailaddress" required="" placeholder="john@deo.com">
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input class="form-control" type="password" required="" id="password" placeholder="Enter your password">
-                    </div>
-
-                    <div class="mb-3">
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="customCheck1">
-                            <label class="form-check-label" for="customCheck1">I accept <a href="#">Terms and Conditions</a></label>
-                        </div> 
+                        <label for="emailaddress" class="form-label">Observacao</label>
+                        <input class="form-control" type="text" id="emailaddress" required="" placeholder="Digite algo que ajude a identificar o pacote">
                     </div>
 
                     <div class="mb-3 text-center">
-                        <button class="btn btn-primary" type="submit">Sign Up Free</button>
+                        <button class="btn btn-primary" type="submit">Cadastrar Pacote</button>
                     </div>
 
                 </form>
