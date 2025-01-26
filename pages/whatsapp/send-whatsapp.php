dd6227239120c8171b5f6dc900649a96a0342651
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-26 21:21:46 +0000

    Deploy

--- pages/whatsapp/send-whatsapp.php
+++ pages/whatsapp/send-whatsapp.php
@@ -4,9 +4,7 @@ require __DIR__ . '/twilio/src/Twilio/autoload.php'; // Ajuste o caminho, se nec
 use Twilio\Rest\Client;
 
 // Suas credenciais do Twilio
-$sid = 'AC1ed2b3fc066e9d0ccfc1126b94283b44'; // Substitua pelo seu Account SID
-$token = '393897c69205733feb5271aea3480923'; // Substitua pelo seu Auth Token
-$twilioNumber = 'whatsapp:+14155238886'; // Número do Twilio Sandbox
+
 
 // Número de destino e mensagem
 $to = 'whatsapp:+5511982734350'; // Substitua pelo número de destino (inclua o código do país)
