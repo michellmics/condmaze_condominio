202f0cdb971dfad2c29f170285de931802e36c8b
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-26 04:09:49 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -30,27 +30,6 @@
 
     $siteAdmin = new SITE_ADMIN();
     $siteAdmin->getParameterInfo();
-    $siteAdmin->getPopupImagePublish(); 
-
-    $qtdePubli = count($siteAdmin->ARRAY_POPUPPUBLISHINFO);
-    if($qtdePubli != 0)
-    {
-        $num = rand(0, $qtdePubli -1);
-        $publiImage = $webmailUrl.$siteAdmin->ARRAY_POPUPPUBLISHINFO[$num]["PUB_DCIMG"];
-
-        if($siteAdmin->ARRAY_POPUPPUBLISHINFO[$num]["PUB_DCLINK"] != "")
-        {
-            $publiImageLink = 'href="' . $siteAdmin->ARRAY_POPUPPUBLISHINFO[$num]["PUB_DCLINK"] . '" target="_blank"';
-        }
-        else
-            {
-                $publiImageLink = "";
-            }        
-    }
-    else
-        {
-            $publiImageLink = "";
-        }
 
     foreach ($siteAdmin->ARRAY_PARAMETERINFO as $item) {
       if ($item['CFG_DCPARAMETRO'] == 'NOME_CONDOMINIO') {
@@ -124,55 +103,7 @@
             }
         }
     </style>
-    <!-- pop-up promoção CSS -->
-    <style>
-        #promoPopup {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: rgba(0, 0, 0, 0.5); /* Fundo escuro semi-transparente */
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            z-index: 1000;
-        }
-
-        .popup-content {
-            position: relative;
-    		background: transparent; /* Alterado para transparente */
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: none;
-            max-width: 90%;
-            max-height: 90%;
-            text-align: center;
-        }
-
-        .popup-content img {
-            max-width: 100%;
-            height: auto;
-        }
-
-        .close-btn {
-    		top: -20px; /* Move o botão para cima da imagem */
-            right: -20px; /* Move o botão para a direita da imagem */
-            position: absolute;
-            background:rgb(0, 0, 0);
-            color: white;
-            border: none;
-            font-size: 20px;
-            padding: 5px 10px;
-            border-radius: 50%;
-            cursor: pointer;
-        }
-
-        .close-btn:hover {
-            background: #cc0000;
-        }
-    </style>
-    <!-- pop-up promoção CSS -->
+   
    
 </head>
 
@@ -198,16 +129,6 @@
             <!-- content -->
 
 
-                <!--  Pop-up publicidade-->
-                <div id="promoPopup" style="display: none;">
-                    <div class="popup-content">
-                        <button class="close-btn" onclick="closePopup()">×</button>
-                        <a <?php echo $publiImageLink; ?>>
-                            <img src="<?php echo $publiImage; ?>" alt="Promoção" style="max-width: 100%; height: auto;">
-                        </a>
-                    </div>
-                </div>
-		        <!--  Pop-up publicidade-->
 
         <!-- ============================================================== -->
         <!-- Start Page Content here -->
