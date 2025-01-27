6eede7329bd32b6116fad54b56bb468648e85adb
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-27 19:56:08 +0000

    Deploy

--- pages/encomendas/index.php
+++ pages/encomendas/index.php
@@ -290,7 +290,7 @@
 
       <!-- ######################################################## --> 
     <!-- SWEETALERT 2 -->   
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script>
 
       function confirmAndSubmit(event) {
