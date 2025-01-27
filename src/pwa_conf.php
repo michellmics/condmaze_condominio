dae55ca6b59a32bb6456c6247fdbd9c600e7c503
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-27 20:06:20 +0000

    Deploy

--- src/pwa_conf.php
+++ src/pwa_conf.php
@@ -1,8 +1,6 @@
-    <!-- PWA CONFIG IMG -->
-    <link rel="icon" href="../../img_pwa/logo_icon.ico" type="image/x-icon">
-    <link rel="shortcut icon" href="../../img_pwa/logo_icon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" href="../../img_pwa/logo_icon.png">
-    <meta name="apple-mobile-web-app-title" content="Hortênsias">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="default">
-    <link rel="shortcut icon" href="../../img_pwa/logo_icon.ico">
+<!-- PWA CONFIG IMG -->
+<link rel="icon" href="../../img_pwa/logo_icon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="../../img_pwa/logo_icon.png">
+<meta name="apple-mobile-web-app-title" content="Hortênsias">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
