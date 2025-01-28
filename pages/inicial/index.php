2b2e9f2b5de1f0939facdec593bab1daecc955e3
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-28 18:11:23 +0000

    Deploy

--- pages/inicial/index.php
+++ pages/inicial/index.php
@@ -180,6 +180,15 @@
 
                     <div id="notifications-container"></div>
 
+                    <div class="toast show align-items-center mb-4" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="d-flex">
+        <div class="toast-body">
+        Hello, world! This is a toast message.
+        </div>
+        <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+</div>
+
                 <!-- Start Content-->
                 <div class="container-fluid">
 
