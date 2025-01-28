91e5519083a8fa1758601127032eeab256244373
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-28 02:07:23 +0000

    Deploy

--- pages/fornecedorAvaliacao/index.php
+++ pages/fornecedorAvaliacao/index.php
@@ -123,8 +123,8 @@
                                                     <h2 class="accordion-header" id="headingOne">
                                                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="display: flex; justify-content: space-between; align-items: center; text-align: left;">
                                                         <div style="display: flex; flex-direction: column;">
-                                                            <strong>Vidraçaria ERK - 36 avaliações</strong>
-                                                            <span>Fone: 19-99275895 (Hortolândia)</span>
+                                                            <strong><?= htmlspecialchars($item['PDS_DCNOME']); ?> - 36 avaliações</strong>
+                                                            <span>Fone: <?= htmlspecialchars($item['PDS_DCTELEFONE']); ?> (<?= htmlspecialchars($item['PDS_DCCIDADE']); ?>)</span>
                                                         </div>                                    
                                                         <div class="rateit rateit-mdi" data-rateit-mode="font" data-rateit-icon="󰓒" data-rateit-value="2.5" data-rateit-ispreset="true" data-rateit-readonly="true" style="margin-left: auto;"></div>
                                                     </button>
