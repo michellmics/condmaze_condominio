4ee65ebe1608c392580cb5a85ec908a76969cf6e
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-28 04:14:50 +0000

    Deploy

--- pages/fornecedorAvaliacao/index.php
+++ pages/fornecedorAvaliacao/index.php
@@ -92,11 +92,6 @@
                         <div class="col-12">
                             <div class="page-title-box">
                                 <div class="page-title-right">
-                                    <ol class="breadcrumb m-0">
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Base UI</a></li>
-                                        <li class="breadcrumb-item active">Accordions & Collapse</li>
-                                    </ol>
                                 </div>
                                 <h4 class="page-title">Avaliação de Prestadores de Serviço</h4>
                             </div>
