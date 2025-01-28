51a9e9255f5d98bca3bce355270c0bc52ab84079
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-28 19:21:30 +0000

    Deploy

--- pages/fornecedorAvaliacao/index.php
+++ pages/fornecedorAvaliacao/index.php
@@ -35,6 +35,7 @@
 <head>
     <meta charset="utf-8" />
     <title><?php echo $nomeCondominio; ?></title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
     <meta content="Coderthemes" name="author" />
