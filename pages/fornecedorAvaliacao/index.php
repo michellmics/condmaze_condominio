a0abfae58bbd64a7be93c00b28e4f27a2175491d
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-28 04:14:14 +0000

    Deploy

--- pages/fornecedorAvaliacao/index.php
+++ pages/fornecedorAvaliacao/index.php
@@ -113,8 +113,7 @@
                                     <div class="tab-content">
                                     <div class="col-sm-5"  style="margin-bottom: 20px;">
                                         <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#cadastrar-modal">Adicionar Empresa</button>
-                                        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#avaliar-modal">Avaliar Empresa</button> 
-                                        <h4 class="header-title">Vidraçarias</h4> 
+                                        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#avaliar-modal">Avaliar Empresa</button>  
                                     </div>
                                         <div class="tab-pane show active" id="default-accordions-preview">
                                             <div class="accordion" id="accordionExample">
