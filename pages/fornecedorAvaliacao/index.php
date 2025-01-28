5bcad1b2b344c564db1e7c75110b50858d97cef4
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-28 04:03:15 +0000

    Deploy

--- pages/fornecedorAvaliacao/index.php
+++ pages/fornecedorAvaliacao/index.php
@@ -145,7 +145,7 @@
                                                                 if(isset($comentario_prestador['USU_DCNOME'])) 
                                                                 {
                                                             ?>
-                                                                    &nbsp;&nbsp;&nbsp;<strong><?= substr(htmlspecialchars($comentario_prestador['USU_DCNOME']),0,15)."..."; ?></strong>                                                                    
+                                                                    &nbsp;&nbsp;&nbsp;<strong><?= substr(htmlspecialchars($comentario_prestador['USU_DCNOME']),0,20)."..."; ?></strong>                                                                    
                                                                     <?php $nota = $comentario_prestador['APS_NMNOTA']; for($aux1 = 0; $aux1 < $nota; $aux1++){echo "<span class='text-success mdi mdi-star'></span>";}?>                                                                 
                                                                     <br>
                                                                     &nbsp;&nbsp;&nbsp;<strong><?= htmlspecialchars($formattedDate); ?></strong> - <?= htmlspecialchars("AP ".$comentario_prestador['USU_DCAPARTAMENTO'])." BL ".htmlspecialchars($comentario_prestador['USU_DCBLOCO']); ?><br>
