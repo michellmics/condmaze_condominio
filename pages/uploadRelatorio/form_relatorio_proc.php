e08cf5475878dd366c1f8416792a328ca571883d
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-29 10:57:57 +0000

    Deploy

--- pages/uploadRelatorio/form_relatorio_proc.php
+++ pages/uploadRelatorio/form_relatorio_proc.php
@@ -676,188 +676,133 @@ if (isset($_FILES['arquivo']) && $_FILES['arquivo']['error'] === UPLOAD_ERR_OK) 
 }
     
 
-
 ?>
 
-<!doctype html>
-<html class="no-js" lang="zxx">
-    <head>
-        <!-- Meta Tags -->
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="keywords" content="Site keywords here">
-		<meta name="description" content="">
-		<meta name='copyright' content=''>
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		
-		<!-- Title -->
-        <title>Condomínio Parque das Hortências</title>
-		
-		<!-- Favicon -->
-        <link rel="icon" href="img/favicon.png">
-		
-		<!-- Google Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
-
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<!-- Nice Select CSS -->
-		<link rel="stylesheet" href="css/nice-select.css">
-		<!-- Font Awesome CSS -->
-        <link rel="stylesheet" href="css/font-awesome.min.css">
-		<!-- icofont CSS -->
-        <link rel="stylesheet" href="css/icofont.css">
-		<!-- Slicknav -->
-		<link rel="stylesheet" href="css/slicknav.min.css">
-		<!-- Owl Carousel CSS -->
-        <link rel="stylesheet" href="css/owl-carousel.css">
-		<!-- Datepicker CSS -->
-		<link rel="stylesheet" href="css/datepicker.css">
-		<!-- Animate CSS -->
-        <link rel="stylesheet" href="css/animate.min.css">
-		<!-- Magnific Popup CSS -->
-        <link rel="stylesheet" href="css/magnific-popup.css">
-		
-		<!-- Medipro CSS -->
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="css/responsive.css">
-
-        <!-- SWEETALERT -->
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-		<link rel="icon" href="https://www.prqdashortensias.com.br/logo_icon.ico" type="image/x-icon">
-    	<link rel="shortcut icon" href="https://www.prqdashortensias.com.br/logo_icon.ico" type="image/x-icon">
-    	<link rel="apple-touch-icon" href="https://www.prqdashortensias.com.br/logo_icon.png">
-    	<meta name="apple-mobile-web-app-title" content="Hortensias">
-    	<meta name="apple-mobile-web-app-capable" content="yes">
-    	<meta name="apple-mobile-web-app-status-bar-style" content="default">
-
-        <style>
-          /* Configuração geral do HTML e Body */
-html, body {
-    height: 100%;
-    margin: 0;
-    display: flex;
-    flex-direction: column;
-}
-
-/* Footer fixo no final */
-.footerNew {
-    background-color: #f0f0f0; /* Cinza bem claro */
-    color: #000; /* Cinza clarinho para o texto */
-    font-size: 12px; /* Fonte pequena */
-    text-align: center; /* Centraliza o texto */
-    padding: 0; /* Remove espaçamento interno */
-    height: 65px; /* Altura do footer */
-    line-height: 30px; /* Centraliza verticalmente o texto */
-    position: fixed; /* Fixa o footer */
-    bottom: 0; /* Colado no final da página */
-    width: 100%; /* Largura total */
-}
- /* Botão de Salvar em lilás */
- .btn-primary {
-      background-color:#8a0653; /* Lilás */
-      border-color: #8a0653; /* Mesma cor da borda */
-      color: white; /* Texto branco */
-  }
-  .btn-primary:hover {
-      background-color: #993399; /* Lilás mais claro ao passar o mouse */
-  }
-
-  /* Botão de Voltar em lilás mais claro */
-  .btn-warning {
-      background-color: #D8BFD8; /* Lilás mais claro */
-      border-color: #D8BFD8; /* Mesma cor da borda */
-      color: #4B0082; /* Texto em roxo escuro */
-  }
-  .btn-warning:hover {
-      background-color: #E6E6FA; /* Lilás ainda mais claro ao passar o mouse */
-  }
-
-</style>
-
-	
-    </head>
-    <body>
-	
-		<!-- Preloader -->
-		<?php include 'src/preloader.php'; ?>
-		<!-- End Preloader -->
-     
-
-		<!-- Header Area -->
-		<?php include 'src/header.php'; ?>
-		<!-- End Header Area -->
 
 
 
-	
-
- 
-
-		<section class="content" style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-    		
-        <?php echo $resultadoParser; ?>
-        <br>
-        <a href="javascript:history.back()">Clique aqui para voltar</a>
 
 
 
-		</section>
 
 
 
-<!-- Footer -->
-<footer class="footerNew">
-<?php include 'src/footer.php'; ?>
-</footer>
-		
 
 
-		
-		<!-- jquery Min JS -->
-        <script src="js/jquery.min.js"></script>
-		<!-- jquery Migrate JS -->
-		<script src="js/jquery-migrate-3.0.0.js"></script>
-		<!-- jquery Ui JS -->
-		<script src="js/jquery-ui.min.js"></script>
-		<!-- Easing JS -->
-        <script src="js/easing.js"></script>
-		<!-- Color JS -->
-		<script src="js/colors.js"></script>
-		<!-- Popper JS -->
-		<script src="js/popper.min.js"></script>
-		<!-- Bootstrap Datepicker JS -->
-		<script src="js/bootstrap-datepicker.js"></script>
-		<!-- Jquery Nav JS -->
-        <script src="js/jquery.nav.js"></script>
-		<!-- Slicknav JS -->
-		<script src="js/slicknav.min.js"></script>
-		<!-- ScrollUp JS -->
-        <script src="js/jquery.scrollUp.min.js"></script>
-		<!-- Niceselect JS -->
-		<script src="js/niceselect.js"></script>
-		<!-- Tilt Jquery JS -->
-		<script src="js/tilt.jquery.min.js"></script>
-		<!-- Owl Carousel JS -->
-        <script src="js/owl-carousel.js"></script>
-		<!-- counterup JS -->
-		<script src="js/jquery.counterup.min.js"></script>
-		<!-- Steller JS -->
-		<script src="js/steller.js"></script>
-		<!-- Wow JS -->
-		<script src="js/wow.min.js"></script>
-		<!-- Magnific Popup JS -->
-		<script src="js/jquery.magnific-popup.min.js"></script>
-		<!-- Counter Up CDN JS -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
-		<!-- Bootstrap JS -->
-		<script src="js/bootstrap.min.js"></script>
-		<!-- Main JS -->
-		<script src="js/main.js"></script>
 
-		
-		
-    </body>
+<!DOCTYPE html>
+<html lang="en" data-layout="topnav">
+
+<head>
+    <meta charset="utf-8" />
+    <title><?php echo $nomeCondominio; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+
+    <!-- Theme Config Js -->
+    <script src="../../assets/js/hyper-config.js"></script>
+
+    <!-- Vendor css -->
+    <link href="../../assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="../../assets/css/app-modern.min.css" rel="stylesheet" type="text/css" id="app-style" />
+
+    <!-- Icons css -->
+    <link href="../../../../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- PWA MOBILE CONF -->
+	<?php include '../../src/pwa_conf.php'; ?>
+	<!-- PWA MOBILE CONF -->
+</head>
+
+<body>
+    <!-- Begin page -->
+    <div class="wrapper">
+
+		<!-- Top bar Area -->
+		<?php include '../../src/top_bar.php'; ?>
+		<!-- End Top bar -->
+
+		<!-- Menu Nav Area -->
+		<?php include '../../src/menu_nav.php'; ?>
+		<!-- End Menu Nav -->
+            <?php
+                $siteAdmin->getListaInfo($userid);
+            ?>
+
+        <div class="content-page">
+            <div class="content">
+                <!-- Start Content-->
+                <div class="container-fluid">
+                </div>
+                <!-- container -->
+            </div>
+            <!-- content -->
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box">
+                                <div class="page-title-right">
+                                </div>
+                                <h4 class="page-title">Enviar Arquivo</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+
+
+                </div>
+                <!-- container -->
+
+            </div>
+            <!-- content -->
+
+            <?php include '../../src/footer_nav.php'; ?>
+
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- END wrapper -->
+
+
+
+    
+
+    <!-- Vendor js -->
+    <script src="../../assets/js/vendor.min.js"></script>
+
+    <!-- Code Highlight js -->
+    <script src="../../assets/vendor/highlightjs/highlight.pack.min.js"></script>
+    <script src="../../assets/vendor/clipboard/clipboard.min.js"></script>
+    <script src="../../assets/js/hyper-syntax.js"></script>
+
+    <!-- Dropzone File Upload js -->
+    <script src="../../assets/vendor/dropzone/dropzone-min.js"></script>
+
+    <!-- File Upload Demo js -->
+    <script src="../../assets/js/ui/component.fileupload.js"></script>
+
+    <!-- App js -->
+    <script src="../../assets/js/app.min.js"></script>
+
+</body>
+
 </html>
+
+
+
+
+
+
+
+
+
+
+
