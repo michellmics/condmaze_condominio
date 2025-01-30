90106edb22e18cbdbd57a92ad284facaec184b5f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-30 03:10:07 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -180,7 +180,7 @@
                                             </select>
 
                                             <span class="input-group-text bg-primary border-primary text-white" id="calendar-icon">
-                                                <i class="mdi mdi-calendar-range font-13"></i>
+                                            <i class="mdi mdi-hand-pointing-right font-15"></i>
                                             </span>
                                         </div>
                                     </form>
