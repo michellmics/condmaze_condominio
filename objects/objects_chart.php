942f9c728ed49fe70e2f6e05abfb65819df0d5ac
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-30 03:06:28 +0000

    Deploy

--- objects/objects_chart.php
+++ objects/objects_chart.php
@@ -285,7 +285,7 @@ ORDER BY
         WHEN 'Jan' THEN 1 WHEN 'Feb' THEN 2 WHEN 'Mar' THEN 3 WHEN 'Apr' THEN 4
         WHEN 'May' THEN 5 WHEN 'Jun' THEN 6 WHEN 'Jul' THEN 7 WHEN 'Aug' THEN 8
         WHEN 'Sep' THEN 9 WHEN 'Oct' THEN 10 WHEN 'Nov' THEN 11 WHEN 'Dec' THEN 12
-    END DESC
+    END ASC
 LIMIT 15;
 ";
 
