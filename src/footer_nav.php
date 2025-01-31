89429841a8326ce2941aa3f462fb786ff6860517
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-31 02:06:03 +0000

    Deploy

--- src/footer_nav.php
+++ src/footer_nav.php
@@ -32,7 +32,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
             </div>
             <div class="modal-body" id="conteudoTermos">
-                sdfdsf
+                <p>hahahahaha</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
@@ -44,30 +44,21 @@
 <footer class="footer">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6 footer-links" >               
-                <a href="https://codemaze.com.br" target="_blank">| <b>Codemaze</b> |</a>  <a href="#" onclick="abrirTermos()">Termo de Privacidade</a>
+            <div class="col-md-6 footer-links">
+                <a href="https://codemaze.com.br" target="_blank">| <b>Codemaze</b> |</a>  
+                <a href="#" onclick="abrirTermos()">Termo de Privacidade</a>
             </div>
             <div class="col-md-6">
-                <div class="text-md-end footer-links d-none d-md-block">
-                    
-                </div>
-            </div>            
-            
+                <div class="text-md-end footer-links d-none d-md-block"></div>
+            </div>
         </div>
     </div>
 </footer>
 
 <script>
     function abrirTermos() {
-        fetch("../termoPrivacidade/termos.php")
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById("conteudoTermos").innerHTML = data;
-                var modal = new bootstrap.Modal(document.getElementById("modalTermos"));
-                modal.show();
-            })
-            .catch(error => console.error("Erro ao carregar os termos:", error));
+        // Exibe o modal com o texto já definido
+        var modal = new bootstrap.Modal(document.getElementById("modalTermos"));
+        modal.show();
     }
 </script>
-
-
