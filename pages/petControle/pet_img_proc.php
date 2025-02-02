33252e143b40eac8a551d620abbbef9abbc3e2f8
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-02 13:13:18 +0000

    Deploy

--- pages/petControle/pet_img_proc.php
+++ pages/petControle/pet_img_proc.php
@@ -80,7 +80,7 @@ foreach ($siteAdmin->ARRAY_HASHIMGINFO as $imgInfo) {
     $distance = hammingDistance($hash1, $hash);
 
     // Ajuste o limiar conforme necessário
-    if ($distance < 15) {
+    if ($distance < 20) {
         // Se a imagem for similar, adiciona as informações no array
         $imagensSemelhantes[] = [
             'nome' => $imgInfo['PEM_DCNOME'],
