f3032da74e00a8a3551a22bb58c1e26ac61a65c8
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-02 03:10:52 +0000

    Deploy

--- pages/petControle/index.php
+++ pages/petControle/index.php
@@ -1,46 +1,338 @@
 <?php
-ini_set('display_errors', 1);  // Habilita a exibição de erros
-error_reporting(E_ALL);        // Reporta todos os erros
+    ini_set('display_errors', 1);  // Habilita a exibição de erros
+    error_reporting(E_ALL);        // Reporta todos os erros
+	include_once "../../objects/objects.php";
+	
+    $siteAdmin = new SITE_ADMIN();  
+    $siteAdmin->getParameterInfo();
 
-function getImageHashGD($imagePath) {
-    $img = imagecreatefromjpeg($imagePath);
-    $img = imagescale($img, 8, 8); // Redimensiona para 8x8
-    imagefilter($img, IMG_FILTER_GRAYSCALE); // Converte para tons de cinza
+    foreach ($siteAdmin->ARRAY_PARAMETERINFO as $item) {
+      if ($item['CFG_DCPARAMETRO'] == 'NOME_CONDOMINIO') {
+          $nomeCondominio = $item['CFG_DCVALOR']; 
+          break; 
+      }
+    }   
 
-    $pixels = [];
-    for ($y = 0; $y < 8; $y++) {
-        for ($x = 0; $x < 8; $x++) {
-            $rgb = imagecolorat($img, $x, $y);
-            $gray = ($rgb >> 16) & 0xFF; // Pega o valor do vermelho (imagem em tons de cinza)
-            $pixels[] = $gray;
-        }
-    }
-
-    $avg = array_sum($pixels) / count($pixels);
-    $hash = '';
-    foreach ($pixels as $pixel) {
-        $hash .= ($pixel >= $avg) ? '1' : '0';
-    }
-
-    imagedestroy($img);
-    return $hash;
-}
-
-function hammingDistance($hash1, $hash2) {
-    return count(array_diff_assoc(str_split($hash1), str_split($hash2)));
-}
-
-// Exemplo de comparação
-$hash1 = getImageHashGD('foto1.jpg');
-$hash2 = getImageHashGD('foto1.jpg');
-
-$distance = hammingDistance($hash1, $hash2);
-if ($distance < 10) { // Ajuste o limiar conforme necessário
-    echo "Imagens semelhantes!";
-} else {
-    echo "Imagens diferentes!";
-}
-
-
-
+    $siteAdmin->getUploadedReportInfo();
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en" data-layout="topnav">
+
+<head>
+    <meta charset="utf-8" />
+    <title><?php echo $nomeCondominio; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+
+    <!-- Theme Config Js -->
+    <script src="../../assets/js/hyper-config.js"></script>
+
+    <!-- Vendor css -->
+    <link href="../../assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="../../assets/css/app-modern.min.css" rel="stylesheet" type="text/css" id="app-style" />
+
+    <!-- Icons css -->
+    <link href="../../../../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Datatables css -->
+    <link href="../../assets/vendor/datatables.net-bs5/css/dataTables.bootstrap5.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/vendor/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/vendor/datatables.net-fixedcolumns-bs5/css/fixedColumns.bootstrap5.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/vendor/datatables.net-fixedheader-bs5/css/fixedHeader.bootstrap5.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/vendor/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/vendor/datatables.net-select-bs5/css/select.bootstrap5.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- SWEETALERT -->
+    <script src="../../js/sweetalert2@11.js"></script>
+
+    <!-- PWA MOBILE CONF -->
+	<?php include '../../src/pwa_conf.php'; ?>
+	<!-- PWA MOBILE CONF -->
+</head>
+
+<body>
+    <!-- Begin page -->
+    <div class="wrapper">
+
+		<!-- Top bar Area -->
+		<?php include '../../src/top_bar.php'; ?>
+		<!-- End Top bar -->
+
+		<!-- Menu Nav Area -->
+		<?php include '../../src/menu_nav.php'; ?>
+		<!-- End Menu Nav -->
+            <?php
+                $siteAdmin->getListaInfo($userid);
+            ?>
+
+        <div class="content-page">
+            <div class="content">
+                <!-- Start Content-->
+                <div class="container-fluid">
+                </div>
+                <!-- container -->
+            </div>
+            <!-- content -->
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box">
+                                <div class="page-title-right">
+                                </div>
+                                <h4 class="page-title"></h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+                    <div class="d-flex justify-content-center mt-5">
+                       <div class="col-md-6">
+                           <div class="card shadow-lg rounded-lg">
+                               <div class="card-body text-center">
+                                   <h4 class="header-title">Pets</h4>
+                                   <p class="text-muted font-14">
+                                       Viu um animalzinho perdido? Verifique aqui se ele pode ser de um morador e o ajude a encontrar o lar :).
+                                   </p>
+
+                                   <form action="pet_img_proc.php" method="POST" enctype="multipart/form-data">
+                                       <div class="mb-3">
+                                           <input name="arquivo" id="arquivo" type="file" accept=".csv" class="form-control" />
+                                       </div>
+
+                                       <div class="d-flex gap-2">
+                                           <select class="form-control" id="mes" name="mes" required>
+                                               <option value="" disabled selected>Tipo</option>
+                                               <option value="janeiro">Cachorro</option>
+                                               <option value="fevereiro">Gato</option>
+                                           </select>
+                                       </div>
+                                           
+                                       <div class="mt-3">
+                                           <button type="submit" class="btn btn-primary">Enviar</button>
+                                       </div>
+                                   </form>
+                               </div>
+
+
+
+
+                               <div class="card">
+                                <div class="card-body">
+                                    <h4 class="header-title">Meus Pets </h4>
+                                    <p class="text-muted font-14">
+                                    </p>
+                                    <div class="tab-content">
+                                        <div class="col-sm-5">                                            
+                                        </div>
+                                        <br>
+                                        <div class="tab-pane show active" id="basic-datatable-preview">
+                                            <table id="basic-datatable" class="table table-striped dt-responsive nowrap w-100">
+                                                <thead>
+                                                    <tr>                                                        
+                                                        <th>NOME</th>
+                                                        <th>TIPO</th>
+                                                        <th>FOTO</th>
+                                                        <th></th>                                                     
+                                                    </tr>
+                                                </thead> 
+                                                <tbody>
+                                                    <?php foreach ($siteAdmin->ARRAY_UPLOADREPORTINFO as $item): ?>
+
+                                                        <?php
+                                                                $date = new DateTime($item['CON_DTINSERT']);
+                                                                $DATA = $date->format('d/m/Y H:i');
+                                                        ?>
+                                                        <tr>                                                       
+                                                            <td style="cursor: pointer;"><?= htmlspecialchars($DATA); ?></td>
+                                                            <td style="cursor: pointer;"><?= htmlspecialchars(strtoupper($item['CON_DCMES_COMPETENCIA_USUARIO'])); ?></td>
+                                                            <td style="cursor: pointer;"><?= htmlspecialchars(strtoupper($item['CON_DCANO_COMPETENCIA_USUARIO'])); ?></td>
+                                                            <td style="cursor: pointer;">
+                                                                <i class="mdi mdi-delete" 
+                                                                   title="Excluir encomenda" 
+                                                                   style="cursor: pointer; font-size: 24px;" 
+                                                                   onclick="confirmDelete(event, 
+                                                                       '<?php echo htmlspecialchars($item['CON_DCMES_COMPETENCIA_USUARIO'], ENT_QUOTES, 'UTF-8'); ?>', 
+                                                                       '<?php echo htmlspecialchars($item['CON_DCANO_COMPETENCIA_USUARIO'], ENT_QUOTES, 'UTF-8'); ?>'
+                                                                   )">
+                                                                </i>
+                                                            </td>
+                                                        </tr>
+                                                    <?php endforeach; ?>
+                                                </tbody>
+                                            </table>
+                                        </div> <!-- end preview-->
+
+
+                                    </div> <!-- end tab-content-->
+
+                                </div> <!-- end card body-->
+                            </div> <!-- end card -->
+
+
+
+
+
+                           </div>
+                       </div>
+                    </div>
+
+                    <!-- end row -->
+
+                </div>
+                <!-- container -->
+
+            </div>
+            <!-- content -->
+
+            <?php include '../../src/footer_nav.php'; ?>
+
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- END wrapper -->
+   <!-- ######################################################## --> 
+    <!-- SWEETALERT 2 -->   
+
+    <script>
+function confirmDelete(event, mes, ano) {
+    //console.log(id);  // Verifica se o id está correto
+    Swal.fire({
+        title: 'Formulário Relatórios',
+        text: "Tem certeza que deseja exluir o relatório?",
+        showDenyButton: true,
+        confirmButtonText: 'CONFIRMAR',
+        denyButtonText: `CANCELAR`,
+        confirmButtonColor: "#536de6",
+        denyButtonColor: "#ff5b5b",
+        width: '400px', // Largura do alerta
+        icon: 'warning',
+        customClass: {
+            title: 'swal-title', // Classe para o título
+            content: 'swal-content', // Classe para o conteúdo (texto)
+            confirmButton: 'swal-confirm-btn',
+            denyButton: 'swal-deny-btn',
+            htmlContainer: 'swal-text'
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Fazer a requisição AJAX
+            $.ajax({
+                url: "deleteReportProc.php", // URL para processamento
+                type: "POST",
+                        data: { 
+                        mes: mes, 
+                        ano: ano 
+                    },
+                success: function (response) {
+                    Swal.fire({
+                        title: 'Atenção',
+                        text: `${response}`,
+                        icon: 'success',
+                        width: '400px', // Largura do alerta
+                        confirmButtonColor: "#536de6",
+                        customClass: {
+                            title: 'swal-title', // Aplicando a mesma classe do título
+                            content: 'swal-content', // Aplicando a mesma classe do texto
+                            htmlContainer: 'swal-text',
+                            confirmButton: 'swal-confirm-btn'
+                        }
+                    }).then(() => {
+                        // Redirecionar ou atualizar a página, se necessário
+                        window.location.href = "index.php";
+                    });
+                },
+                error: function (xhr, status, error) {
+                    Swal.fire({
+                        title: 'Erro!',
+                        text: 'Erro ao excluir a avaliação.',
+                        icon: 'error',
+                        width: '400px', // Largura do alerta
+                        confirmButtonColor: "#536de6",
+                        customClass: {
+                            title: 'swal-title', // Aplicando a mesma classe do título
+                            content: 'swal-content', // Aplicando a mesma classe do texto
+                            htmlContainer: 'swal-text',
+                            confirmButton: 'swal-confirm-btn'
+                        }
+                    });
+                }
+            });
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            Swal.fire('Cancelado', 'Nenhuma alteração foi salva.', 'info');
+        }
+    });
+}
+
+$(document).ready(function () {
+    // Não é necessário associar a função ao botão de submit, pois ela já está sendo chamada no clique do ícone.
+});
+</script>
+<style>
+  /* Estilos para aumentar o tamanho da fonte */
+  .swal-title {
+    font-size: 25px !important; /* Tamanho maior para o título */
+  }
+
+  .swal-text {
+    font-size: 16px !important; /* Tamanho maior para o conteúdo */
+  }
+
+  /* Aumentar o tamanho dos textos dos botões */
+  .swal-confirm-btn,
+  .swal-deny-btn,
+  .swal-cancel-btn {
+    font-size: 16px !important; /* Tamanho maior para os textos dos botões */
+    padding: 8px 8px !important; /* Aumenta o espaço ao redor do texto */
+  }
+</style>
+<!-- ######################################################## --> 
+<!-- SWEETALERT 2 -->   
+
+
+    <!-- Datatables js -->
+    <script src="../../assets/vendor/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-fixedcolumns-bs5/js/fixedColumns.bootstrap5.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-buttons/js/buttons.html5.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-buttons/js/buttons.flash.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-buttons/js/buttons.print.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-select/js/dataTables.select.min.js"></script>
+
+    <!-- Datatable Demo Aapp js -->
+    <script src="../../assets/js/pages/demo.datatable-init.js?ver=<?php echo time(); ?>"></script>
+
+    <!-- Vendor js -->
+    <script src="../../assets/js/vendor.min.js"></script>
+
+    <!-- Code Highlight js -->
+    <script src="../../assets/vendor/highlightjs/highlight.pack.min.js"></script>
+    <script src="../../assets/vendor/clipboard/clipboard.min.js"></script>
+    <script src="../../assets/js/hyper-syntax.js"></script>
+
+    <!-- Dropzone File Upload js -->
+    <script src="../../assets/vendor/dropzone/dropzone-min.js"></script>
+
+    <!-- File Upload Demo js -->
+    <script src="../../assets/js/ui/component.fileupload.js"></script>
+
+    <!-- App js -->
+    <script src="../../assets/js/app.min.js"></script>
+
+</body>
+
+</html>
