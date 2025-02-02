41c69a8e972e2dc6f466af8dbf25bcc8ef7533ba
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-02 04:34:13 +0000

    Deploy

--- pages/petControle/index.php
+++ pages/petControle/index.php
@@ -201,12 +201,8 @@
                                                         <tr>                                                       
                                                             <td style="cursor: pointer;"><?= htmlspecialchars(strtoupper($item['PEM_DCNOME'])); ?></td>
                                                             <td style="cursor: pointer;"><?= htmlspecialchars(strtoupper($item['PEM_DCTIPO'])); ?></td>
-
-                                                            <td style="cursor: pointer;">
-                                                            <a class="pe-3" href="#">
-                                                                <img src="<?= htmlspecialchars(strtoupper($item['PET_DCPATHFOTO'])); ?>" class="avatar-sm rounded-circle" alt="Generic placeholder image">
-                                                            </a>
-                                                            </td>
+                                                            <td style="cursor: pointer;"><?= htmlspecialchars(strtoupper($item['PET_DCPATHFOTO'])); ?></td>
+         
                                                             
                                                             
                                                             <td style="cursor: pointer;">
