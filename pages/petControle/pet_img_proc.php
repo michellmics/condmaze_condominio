2d81c3ac300f8123260f237af127fbd22387acde
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-02 16:35:45 +0000

    Deploy

--- pages/petControle/pet_img_proc.php
+++ pages/petControle/pet_img_proc.php
@@ -82,7 +82,7 @@ foreach ($siteAdmin->ARRAY_HASHIMGINFO as $imgInfo) {
     $distance = hammingDistance($hash1, $hash);  // Calcula a distância de Hamming
 
     // Ajuste o limiar conforme necessário
-    if ($distance < 10) {  // Se a distância for menor que 35, considera como semelhante
+    if ($distance < 15) {  // Se a distância for menor que 35, considera como semelhante
         // Se a imagem for similar, adiciona as informações no array
         $imagensSemelhantes[] = [
             'nome' => $imgInfo['PEM_DCNOME'],
@@ -90,7 +90,7 @@ foreach ($siteAdmin->ARRAY_HASHIMGINFO as $imgInfo) {
             'tutor' => "TUTOR",  // Ajuste conforme necessário
             'raca' => $imgInfo['PEM_DCRACA'],
             'img' => $imgInfo['PET_DCPATHFOTO']
-        ];   
+        ];    
     }
 }
  
