2c938b53dee54cb95044d279cd414844c9f55f34
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-03 01:04:26 +0000

    Deploy

--- pages/churrascometro/index.php
+++ pages/churrascometro/index.php
@@ -77,6 +77,10 @@
                 $metodo = "update";   
             }       
 
+            $carneNecessariaMulher = $siteAdmin->ARRAY_LISTAEVENTOSINFO["LEU_DCCONVIDADO_MULHER"] * 0.350;
+            $carneNecessariaHomem = $siteAdmin->ARRAY_LISTAEVENTOSINFO["LEU_DCCONVIDADO_HOMEM"] * 0.500;
+            $carneNecessaria = $carneNecessariaMulher + $carneNecessariaHomem;
+
         ?>
 
         <div class="content-page">
@@ -124,11 +128,11 @@
                                         <div class="row">
                                             <div class="col-3 mb-3">
                                                 <label for="qtdehomem" class="form-label">Qtde Homens</label>
-                                                <input class="form-control" id="qtdehomem" type="number" value="<?php echo $siteAdmin->ARRAY_LISTAEVENTOSINFO["LEU_DCCONVIDADO_HOMEM"] ?>" name="qtdehomem"  step="1" min="0">
+                                                <input class="form-control" id="qtdehomem" type="number" value="<?php echo $siteAdmin->ARRAY_LISTAEVENTOSINFO["LEU_DCCONVIDADO_HOMEM"]; ?>" name="qtdehomem"  step="1" min="0">
                                             </div>
                                             <div class="col-3 mb-3">
                                                 <label for="qtdemulher" class="form-label">Qtde Mulheres</label>
-                                                <input class="form-control" id="qtdemulher" value="<?php echo $siteAdmin->ARRAY_LISTAEVENTOSINFO["LEU_DCCONVIDADO_MULHER"] ?>" type="number" name="qtdemulher"  step="1" min="0">
+                                                <input class="form-control" id="qtdemulher" value="<?php echo $siteAdmin->ARRAY_LISTAEVENTOSINFO["LEU_DCCONVIDADO_MULHER"]; ?>" type="number" name="qtdemulher"  step="1" min="0">
                                             </div>
                                             <div class="col-3 mb-3">
                                                 <label for="qtdehomem" class="form-label">Fator C. Homem</label>
@@ -141,8 +145,8 @@
                                         </div>
                                         <div class="row">
                                             <div class="col-4 mb-3">
-                                                <label for="qtdehomem" class="form-label">Carne Necessária</label>
-                                                <input readonly class="form-control" id="carnenecessaria" type="number" name="carnenecessaria" style="background-color:rgb(10, 10, 10); color: white;">
+                                                <label for="qtdehomem" class="form-label">Carne Necessária</label> 
+                                                <input readonly class="form-control" id="carnenecessaria" type="number" value="<?php echo $carneNecessaria; ?>" name="carnenecessaria" style="background-color:rgb(10, 10, 10); color: white;">
                                             </div>
                                             <div class="col-4 mb-3">
                                                 <label for="qtdemulher" class="form-label">Carne Calculada</label>
