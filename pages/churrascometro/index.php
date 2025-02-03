16a37a072f1285198b3929e8cd31a2b67d60a498
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-03 01:30:08 +0000

    Deploy

--- pages/churrascometro/index.php
+++ pages/churrascometro/index.php
@@ -87,7 +87,7 @@
          
             foreach($siteAdmin->ARRAY_LISTAEVENTOSINFO as $item)
             {
-                if($item["LEV_DCTIPO"] == "1"){$carneCalculada =  $carneCalculada += (float) $item["LEV_DCQTDE"];}       
+                echo $item["LEV_DCTIPO"];          
             }
            
 
