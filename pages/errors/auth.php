2c4ebe495ec06bbe790e0084a4fcad60c6de7bc5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-05 03:19:37 +0000

    Deploy

---
+++ pages/errors/auth.php
@@ -0,0 +1,137 @@
+<?php
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
+	include_once "../../objects/objects.php";
+    $siteAdmin = new SITE_ADMIN();  
+    $siteAdmin->getParameterInfo();
+
+    foreach ($siteAdmin->ARRAY_PARAMETERINFO as $item) {
+        if ($item['CFG_DCPARAMETRO'] == 'NOME_CONDOMINIO') {
+            $nomeCondominio = $item['CFG_DCVALOR']; 
+            break; 
+        }
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" data-layout-mode="detached" data-topbar-color="dark" data-menu-color="light" data-sidenav-user="true">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Error 404 | Hyper - Responsive Bootstrap 5 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../../assets/images/favicon.ico">
+
+    <!-- Theme Config Js -->
+    <script src="../../assets/js/hyper-config.js"></script>
+
+    <!-- Vendor css -->
+    <link href="../../assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="../../assets/css/app-modern.min.css" rel="stylesheet" type="text/css" id="app-style" />
+
+    <!-- Icons css -->
+    <link href="../../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- PWA MOBILE CONF -->
+	<?php include '../../src/pwa_conf.php'; ?>
+	<!-- PWA MOBILE CONF -->
+
+</head>
+
+<body>
+<span class="loader"></span> 
+    <!-- Begin page -->
+    <div class="wrapper">
+
+        
+        <!-- ========== Topbar Start ========== -->
+		<!-- Top bar Area -->
+		<?php include '../../src/top_bar.php'; ?>
+
+        <!-- ========== Topbar End ========== -->
+
+		<!-- Menu Nav Area -->
+		<?php include '../../src/menu_nav.php'; ?>
+		<!-- End Menu Nav -->
+        
+
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+
+        <div class="content-page">
+            <div class="content">
+
+                <!-- Start Content-->
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box">
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                                        <li class="breadcrumb-item active">404 Error</li>
+                                    </ol>
+                                </div>
+                                <h4 class="page-title">404 Error</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4">
+                            <div class="text-center">
+                                <img src="../../assets/images/svg/file-searching.svg" height="90" alt="File not found Image">
+
+                                <h1 class="text-error mt-4">404</h1>
+                                <h4 class="text-uppercase text-danger mt-3">Page Not Found</h4>
+                                <p class="text-muted mt-3">It's looking like you may have taken a wrong turn. Don't worry... it
+                                    happens to the best of us. Here's a
+                                    little tip that might help you get back on track.</p>
+
+                                <a class="btn btn-info mt-3" href="index.html"><i class="mdi mdi-reply"></i> Return Home</a>
+                            </div> <!-- end /.text-center-->
+                        </div> <!-- end col-->
+                    </div>
+                    <!-- end row -->
+
+                </div> <!-- container -->
+
+            </div> <!-- content -->
+
+            <!-- Footer Start -->
+		    <?php include '../../src/footer_nav.php'; ?>
+            <!-- end Footer -->
+
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+
+    </div>
+    <!-- END wrapper -->
+
+    <!-- Vendor js -->
+    <script src="../../assets/js/vendor.min.js"></script>
+
+    <!-- App js -->
+    <script src="../../assets/js/app.min.js"></script>
+
+</body>
+
+</html>
