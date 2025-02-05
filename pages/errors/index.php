2441c5df8d8e2882acca057e58d18f62ed218743
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-05 03:55:39 +0000

    Deploy

--- pages/errors/index.php
+++ pages/errors/index.php
@@ -197,9 +197,7 @@ function mostrarImagem(src) {
 
                     <div class="d-flex justify-content-center mt-4">
                        <div class="col-md-6">
-                           <div class="card shadow-lg rounded-lg">
-                               
-                           <div class="row justify-content-center align-items-center vh-100">
+                       <div class="row justify-content-center align-items-center vh-100">
                         <div class="col-lg-12">
                             <div class="text-center">
                                 <img src="../../assets/images/svg/file-searching.svg" height="90" alt="File not found Image">
@@ -212,8 +210,6 @@ function mostrarImagem(src) {
                             </div> <!-- end /.text-center-->
                         </div> <!-- end col-->
                     </div>
-
-                           </div>
                        </div>
                     </div>
 
