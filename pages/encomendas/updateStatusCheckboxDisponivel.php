f0fc72e595ea9fd9daad7d1c4a305d48a83e2b90
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-12 23:57:27 +0000

    Deploy

--- pages/encomendas/updateStatusCheckboxDisponivel.php
+++ pages/encomendas/updateStatusCheckboxDisponivel.php
@@ -16,7 +16,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $result = $siteAdmin->updateCheckboxEncomendasDisponivelMorador($id, $status, $telefone);
         if($status == "DISPONIVEL")
         {
-            $msg = "Olá *$nome*, sua entrega com ID *$id* está disponível para retirada na portaria do *Condomínio Parque das Hortênsias.*";
+            $msg = "Olá *$nome*, sua entrega com ID *$id* está disponível para retirada na portaria do *Condomínio Parque das Hortênsias.*
+                    \nAo chegar na portaria, acesse o link abaixo para liberar a retirada.\n\n
+                    link aqui.";
            // $telefone = "11982734350";
             $result = $siteAdmin->whatsapp($msg,$telefone);
         }
