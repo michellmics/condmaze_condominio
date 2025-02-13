02eb63a118cd8febca07451a38f60241a6b57c29
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-13 17:36:48 +0000

    Deploy

--- objects/objects.php
+++ objects/objects.php
@@ -41,7 +41,7 @@
         {
             	$host = $_ENV['ENV_BD_HOST'];
             	$dbname = $_ENV['ENV_BD_DATABASE']; 
-            	$user = $_ENV['ENV_BD_USER'];
+            	$user = $_ENV['ENV_BD_USE'];
             	$pass = $_ENV['ENV_BD_PASS'];
 		
             try {
