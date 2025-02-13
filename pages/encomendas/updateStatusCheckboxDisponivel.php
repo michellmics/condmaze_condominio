7d7366d5b69a553922a33bf0f92d6f717d2705ee
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-13 01:07:09 +0000

    Deploy

--- pages/encomendas/updateStatusCheckboxDisponivel.php
+++ pages/encomendas/updateStatusCheckboxDisponivel.php
@@ -19,8 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         {
             $link = "https://parquedashortensias.codemaze.com.br/api_encomenda.php?hash=$hash";
             $msg = "Olá *$nome*, sua entrega com ID *$id* está disponível para retirada na portaria do *Condomínio Parque das Hortênsias.*
-                    \nAo chegar na portaria, acesse o link abaixo para liberar a retirada.\n\n
-                    $link";
+                    \nAo chegar na portaria, acesse o link abaixo para liberar a retirada.\n[CLIQUE AQUI PARA LIBERAR A RETIRADA]($link)";
            // $telefone = "11982734350";
             $result = $siteAdmin->whatsapp($msg,$telefone);
         }
