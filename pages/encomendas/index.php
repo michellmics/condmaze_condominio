0d5517ef4ba395db41d0c9952ab5356e7edf48e7
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-13 03:10:40 +0000

    Deploy

--- pages/encomendas/index.php
+++ pages/encomendas/index.php
@@ -318,47 +318,6 @@
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
-<script>
-    $(document).ready(function() {
-        var table = $('#basic-datatable').DataTable({
-            responsive: {
-                details: {
-                    type: 'column',
-                    target: 0
-                }
-            },
-            columnDefs: [
-                { className: 'dt-control', orderable: false, targets: 0 }
-            ],
-            order: [[1, 'desc']]
-        });
-
-        // Ao clicar no "+", mostra detalhes escondidos
-        $('#basic-datatable tbody').on('click', 'td.dt-control', function () {
-            var tr = $(this).closest('tr');
-            var row = table.row(tr);
-
-            if (row.child.isShown()) {
-                row.child.hide();
-                tr.removeClass('shown');
-            } else {
-                row.child(format(row.data())).show();
-                tr.addClass('shown');
-            }
-        });
-
-        // Função que exibe detalhes adicionais
-        function format(d) {
-            return '<table class="table table-bordered" style="width:100%;">' +
-                '<tr><td><b>ID:</b></td><td>' + d[2] + '</td></tr>' +
-                '<tr><td><b>Nome:</b></td><td>' + d[4] + '</td></tr>' +
-                '<tr><td><b>Telefone:</b></td><td>' + d[5] + '</td></tr>' +
-                '<tr><td><b>Observação:</b></td><td>' + d[7] + '</td></tr>' +
-                '<tr><td><b>Data de Entrega:</b></td><td>' + d[6] + '</td></tr>' +
-            '</table>';
-        }
-    });
-</script>
 
    <!-- ######################################################## --> 
     <!-- SWEETALERT 2 -->   
@@ -658,11 +617,12 @@ $(document).ready(function () {
     <script src="../../assets/vendor/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js"></script>
     <script src="../../assets/vendor/datatables.net-buttons/js/buttons.html5.min.js"></script>
     <script src="../../assets/vendor/datatables.net-buttons/js/buttons.flash.min.js"></script>
+    <script src="../../assets/vendor/datatables.net-buttons/js/buttons.print.min.js"></script>
     <script src="../../assets/vendor/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
     <script src="../../assets/vendor/datatables.net-select/js/dataTables.select.min.js"></script>
 
     <!-- Datatable Demo Aapp js -->
-    <script src="../../assets/js/pages/demo.datatable-init.js"></script>
+    <script src="../../assets/js/pages/demo.datatable-init.js?ver=<?php echo time(); ?>"></script>
 
 </body>
 
