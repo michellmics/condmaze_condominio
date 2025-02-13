6088a866413d9413bf75c0dd394a94873c0cc6ca
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-13 01:00:25 +0000

    Deploy

--- api_encomenda.php
+++ api_encomenda.php
@@ -32,7 +32,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mensagem de Status</title>
+    <title>Liberação de Encomenda</title>
+
+    <!-- Ícones do PWA -->
+    <link rel="icon" href="img_pwa/logo_icon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="img_pwa/logo_icon.png">
+
+    <!-- Configurações do PWA -->
+    <meta name="apple-mobile-web-app-title" content="Hortênsias">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Manifest JSON -->
+    <link rel="manifest" href="/manifest.json">
+
     <style>
         /* Estilos gerais */
         body {
