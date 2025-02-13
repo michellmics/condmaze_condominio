78d0069e5f089cbfb97bbff33c882833ae256049
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-13 03:54:17 +0000

    Deploy

--- pages/encomendas/index.php
+++ pages/encomendas/index.php
@@ -195,7 +195,7 @@
                                                         
                                                     ?>
                                                     <tr>    
-                                                        <!--<td class="align-middle" style="font-size: 12px;"><?= //htmlspecialchars($dataPortaria); ?></td> -->
+                                                        <!--<td class="align-middle" style="font-size: 12px;">//htmlspecialchars($dataPortaria); </td> -->
                                                         <td class="align-middle" style="font-size: 12px;"><?= htmlspecialchars($item['ENC_IDENCOMENDA']); ?></td>
                                                         <td class="align-middle" style="font-size: 12px;"><?= htmlspecialchars($item['USU_DCAPARTAMENTO']); ?></td>
                                                         <td class="align-middle" nome="<?= htmlspecialchars($item['USU_DCNOME']); ?>" style="font-size: 12px; word-wrap: break-word;"><?= htmlspecialchars(substr($item['USU_DCNOME'],0,21)."..."); ?></td>    
