3c2383c3fc3a73debd278b6bfde5406801f2fa23
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-13 17:03:47 +0000

    Deploy

--- objects/objects.php
+++ objects/objects.php
@@ -66,8 +66,7 @@
                 $stmt->execute();
                 return $stmt->fetchAll(PDO::FETCH_ASSOC);
             } catch (PDOException $e) {
-                //return ["error" => $e->getMessage()];
-                return "Deu Ruim";
+                return ["error" => $e->getMessage()];
             }       
         }
         public function getAvaliacoesByPrestador($PDS_IDPRESTADOR_SERVICO)
