5ced7db0d14cfa346e7f985d84d6d35d879c17be
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-13 03:41:51 +0000

    Deploy

--- pages/encomendas/index_bkp.php
+++ pages/encomendas/index_bkp.php
@@ -55,10 +55,6 @@
     <!-- Datatables css -->
     <link href="../../assets/vendor/datatables.net-bs5/css/dataTables.bootstrap5.min.css" rel="stylesheet" type="text/css" />
     <link href="../../assets/vendor/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-    <link href="../../assets/vendor/datatables.net-fixedcolumns-bs5/css/fixedColumns.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-    <link href="../../assets/vendor/datatables.net-fixedheader-bs5/css/fixedHeader.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-    <link href="../../assets/vendor/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-    <link href="../../assets/vendor/datatables.net-select-bs5/css/select.bootstrap5.min.css" rel="stylesheet" type="text/css" />
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="../../assets/images/favicon.ico">
@@ -82,6 +78,7 @@
     <!-- PWA MOBILE CONF -->
 	<?php include '../../src/pwa_conf.php'; ?>
 	<!-- PWA MOBILE CONF -->
+
 </head>
 
 <body>
@@ -105,7 +102,6 @@
             </div>
             <!-- content -->
 
-
                 <!-- Start Content-->
                 <div class="container-fluid">
 
@@ -114,14 +110,15 @@
                         <div class="col-12">
                             <div class="page-title-box">
                                 <div class="page-title-right">
+                                
                                 </div>
-                                <h4 class="page-title">Encomendas</h4>
                             </div>
                         </div>
                     </div>
                     <!-- end page title -->
 
                     <div class="row">
+
                     <div class="col-lg-12">
                             <div class="card">
                                 <div class="card-body">
@@ -267,22 +264,333 @@
                             </div> <!-- end card -->
                         </div><!-- end col-->
                     </div>
-                    </div> <!-- end row-->
-
+                    <!-- end row-->
                 </div> <!-- container -->
-
-            </div> <!-- content -->
-
-            <?php include '../../src/footer_nav.php'; ?>
-
+		<!-- Menu Nav Area -->
+		<?php include '../../src/footer_nav.php'; ?>
+		<!-- End Menu Nav -->
         </div>
 
         <!-- ============================================================== -->
         <!-- End Page content -->
         <!-- ============================================================== -->
 
-
     </div>
+    <!-- END wrapper -->
+
+
+<!-- cadastrar pacote modal-->
+<div id="signup-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <div class="modal-body">
+                <div class="text-center mt-2 mb-4">
+                    <a href="index.html" class="text-success">
+                        <span>Cadastro de Pacote</span>
+                    </a>
+                </div>
+
+                <form class="needs-validation" novalidate id="form" role="form" method="POST">
+
+                    <div class="mb-3">
+                        <label for="apartamento" class="form-label">Apartamento</label>
+                        <input class="form-control" type="number" name="apartamento" id="apartamento" maxlength="4" required="" placeholder="Digite o número do apartamento">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="observacao" class="form-label">Observacao</label>
+                        <input class="form-control" type="text" id="observacao" name="observacao" required="" maxlength="50" placeholder="Digite algo que ajude a identificar o pacote" style="text-transform: uppercase;">
+                    </div>
+
+                    <div class="mb-3 text-center">
+                        <button class="btn btn-primary" id="botao" type="button">Cadastrar Pacote</button>
+                    </div>
+
+                </form>
+
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+   <!-- ######################################################## --> 
+    <!-- SWEETALERT 2 -->   
+
+    <script>
+function confirmDelete(event, id) {
+    console.log(id);  // Verifica se o id está correto
+    Swal.fire({
+        title: 'Formulário Encomendas',
+        text: "Tem certeza que deseja exluir a encomenda?",
+        showDenyButton: true,
+        confirmButtonText: 'CONFIRMAR',
+        denyButtonText: `CANCELAR`,
+        confirmButtonColor: "#536de6",
+        denyButtonColor: "#ff5b5b",
+        width: '400px', // Largura do alerta
+        icon: 'warning',
+        customClass: {
+            title: 'swal-title', // Classe para o título
+            content: 'swal-content', // Classe para o conteúdo (texto)
+            confirmButton: 'swal-confirm-btn',
+            denyButton: 'swal-deny-btn',
+            htmlContainer: 'swal-text'
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Fazer a requisição AJAX
+            $.ajax({
+                url: "deleteEncomendaProc.php", // URL para processamento
+                type: "POST",
+                data: { id: id },  // Enviando o ID via POST
+                success: function (response) {
+                    Swal.fire({
+                        title: 'Atenção',
+                        text: `${response}`,
+                        icon: 'success',
+                        width: '400px', // Largura do alerta
+                        confirmButtonColor: "#536de6",
+                        customClass: {
+                            title: 'swal-title', // Aplicando a mesma classe do título
+                            content: 'swal-content', // Aplicando a mesma classe do texto
+                            htmlContainer: 'swal-text',
+                            confirmButton: 'swal-confirm-btn'
+                        }
+                    }).then(() => {
+                        // Redirecionar ou atualizar a página, se necessário
+                        window.location.href = "index.php";
+                    });
+                },
+                error: function (xhr, status, error) {
+                    Swal.fire({
+                        title: 'Erro!',
+                        text: 'Erro ao excluir a avaliação.',
+                        icon: 'error',
+                        width: '400px', // Largura do alerta
+                        confirmButtonColor: "#536de6",
+                        customClass: {
+                            title: 'swal-title', // Aplicando a mesma classe do título
+                            content: 'swal-content', // Aplicando a mesma classe do texto
+                            htmlContainer: 'swal-text',
+                            confirmButton: 'swal-confirm-btn'
+                        }
+                    });
+                }
+            });
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            Swal.fire('Cancelado', 'Nenhuma alteração foi salva.', 'info');
+        }
+    });
+}
+
+$(document).ready(function () {
+    // Não é necessário associar a função ao botão de submit, pois ela já está sendo chamada no clique do ícone.
+});
+</script>
+<style>
+  /* Estilos para aumentar o tamanho da fonte */
+  .swal-title {
+    font-size: 25px !important; /* Tamanho maior para o título */
+  }
+
+  .swal-text {
+    font-size: 16px !important; /* Tamanho maior para o conteúdo */
+  }
+
+  /* Aumentar o tamanho dos textos dos botões */
+  .swal-confirm-btn,
+  .swal-deny-btn,
+  .swal-cancel-btn {
+    font-size: 16px !important; /* Tamanho maior para os textos dos botões */
+    padding: 8px 8px !important; /* Aumenta o espaço ao redor do texto */
+  }
+</style>
+<!-- ######################################################## --> 
+<!-- SWEETALERT 2 -->   
+
+
+
+
+
+
+     
+
+      <!-- ######################################################## --> 
+    <!-- SWEETALERT 2 -->   
+    <script src="../../js/jquery-3.5.1.min.js"></script>
+    <script>
+
+      function confirmAndSubmit(event) {
+
+        event.preventDefault(); // Impede o envio padrão do formulário
+        Swal.fire({
+          title: 'Cadastro de Pacote',
+          text: "Tem certeza que deseja cadastrar um pacote?",
+          showDenyButton: true,
+          confirmButtonText: 'CONFIRMAR',
+          denyButtonText: `CANCELAR`,
+          confirmButtonColor: "#536de6",
+          denyButtonColor: "#ff5b5b",
+          width: '400px', // Largura do alerta
+          icon: 'warning',
+          customClass: {
+            title: 'swal-title', // Classe para o título
+            content: 'swal-content', // Classe para o conteúdo (texto)
+            confirmButton: 'swal-confirm-btn',
+            denyButton: 'swal-deny-btn',
+            htmlContainer: 'swal-text'
+          }
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // Capturar os dados do formulário
+            var formData = new FormData($("#form")[0]); // Usa o FormData para enviar arquivos
+            // Fazer a requisição AJAX
+            $.ajax({
+              url: "insertPacoteProc.php", // URL para processamento
+              type: "POST",
+              data: formData,
+              processData: false, // Impede o jQuery de processar os dados
+              contentType: false, // Impede o jQuery de definir o tipo de conteúdo
+              success: function (response) {
+                Swal.fire({
+              title: 'Atenção',
+              text: `${response}`,
+              icon: 'success',
+              width: '400px', // Largura do alerta
+              confirmButtonColor: "#536de6",
+              customClass: {
+                title: 'swal-title', // Aplicando a mesma classe do título
+                content: 'swal-content', // Aplicando a mesma classe do texto
+                htmlContainer: 'swal-text',
+                confirmButton: 'swal-confirm-btn'
+              }
+            }).then(() => {
+                  // Redirecionar ou atualizar a página, se necessário
+                   window.location.href = "index.php";
+                });
+              },
+              error: function (xhr, status, error) {
+                Swal.fire({
+              title: 'Erro!',
+              text: 'Erro ao atualizar o convidado.'.error,
+              icon: 'error',
+              width: '400px', // Largura do alerta
+              confirmButtonColor: "#536de6",
+              customClass: {
+                title: 'swal-title', // Aplicando a mesma classe do título
+                content: 'swal-content', // Aplicando a mesma classe do texto
+                htmlContainer: 'swal-text',
+                confirmButton: 'swal-confirm-btn'
+              }
+            });
+              },
+            });
+          } else if (result.dismiss === Swal.DismissReason.cancel) {
+            Swal.fire('Cancelado', 'Nenhuma alteração foi salva.', 'info');
+          }
+        });
+      }
+      // Associar a função ao botão de submit
+      $(document).ready(function () {
+        $("#botao").on("click", confirmAndSubmit);
+      });
+</script> 
+<style>
+  /* Estilos para aumentar o tamanho da fonte */
+  .swal-title {
+    font-size: 25px !important; /* Tamanho maior para o título */
+  }
+
+  .swal-text {
+    font-size: 16px !important; /* Tamanho maior para o conteúdo */
+  }
+
+  /* Aumentar o tamanho dos textos dos botões */
+  .swal-confirm-btn,
+  .swal-deny-btn,
+  .swal-cancel-btn {
+    font-size: 16px !important; /* Tamanho maior para os textos dos botões */
+    padding: 8px 8px !important; /* Aumenta o espaço ao redor do texto */
+  }
+</style>
+<!-- ######################################################## --> 
+<!-- SWEETALERT 2 --> 
+
+    <script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const switches = document.querySelectorAll('input[type="checkbox"][data-switch="success"]');
+        
+        switches.forEach(switchElem => {
+            switchElem.addEventListener('change', function () {
+                const id = this.getAttribute('data-id');
+                const status = this.checked ? 'DISPONIVEL' : 'INDISPONIVEL';
+                const td = document.querySelector('td[nome]');
+                const tdTelefone = document.querySelector('td[telefone]');
+                const nome = td.getAttribute('nome');
+                const telefone = tdTelefone.getAttribute('telefone');            
+                const tdHash = document.querySelector('td[hash]');
+                const hash = tdHash.getAttribute('hash');
+
+                // Envia a alteração para o servidor
+                fetch('updateStatusCheckboxDisponivel.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ id, status, nome, telefone, hash })
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (!data.success) {
+                        //alert('Erro ao atualizar o status!');
+                    }
+                    window.location.href = "index.php";
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                    //alert('Erro ao comunicar com o servidor.');
+                });
+            });
+        });
+    });
+    </script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const switches = document.querySelectorAll('input[type="checkbox"][data-switch="success"]');
+        
+        switches.forEach(switchElem => {
+            switchElem.addEventListener('change', function () {
+                const id = this.getAttribute('data-id1');
+                const status = this.checked ? 'ENTREGUE' : 'PENDENTE';
+
+                // Envia a alteração para o servidor
+                fetch('updateStatusCheckboxEntregar.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ id, status })
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (!data.success) {
+                        //alert('Erro ao atualizar o status!');
+                    }
+                    window.location.href = "index.php";
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                    alert('Erro ao comunicar com o servidor.');
+                });
+            });
+        });
+    });
+    </script>
+
+
 
     <!-- Vendor js -->
     <script src="../../assets/js/vendor.min.js"></script>
@@ -304,25 +612,14 @@
     <!-- App js -->
     <script src="../../assets/js/app.min.js"></script>
 
+
     <!-- Datatables js -->
-    <script src="../../assets/vendor/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-fixedcolumns-bs5/js/fixedColumns.bootstrap5.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-buttons/js/buttons.flash.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-select/js/dataTables.select.min.js"></script>
-
-    <!-- Datatable Demo Aapp js -->
-    <script src="../../assets/js/pages/demo.datatable-init.js?ver=<?php echo time(); ?>"></script>
-
-
+    <script src="assets/vendor/datatables.net/js/dataTables.min.js"></script>
+    <script src="assets/vendor/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="assets/vendor/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="assets/vendor/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
+    <!-- Datatable Init js -->
+    <script src="assets/js/pages/demo.datatable-init.js"></script>
 
 </body>
 
