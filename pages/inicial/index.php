034007ee2b9af88576e7d29f7963e6b01b2515f6
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-14 00:47:35 +0000

    Deploy

--- pages/inicial/index.php
+++ pages/inicial/index.php
@@ -314,8 +314,9 @@
                 <h5 class="modal-title" id="scrollableModalTitle">Modal title</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
             </div>
-            <div class="modal-body" id="modal-body-content">
-            <div id="modal-file-link"></div>
+            <div class="modal-body">
+            <div id="modal-body-content"></div> <!-- Conteúdo do artigo -->
+            <div id="modal-file-link"></div> <!-- Link para download do arquivo -->
                
             </div>
             <div class="modal-footer">
