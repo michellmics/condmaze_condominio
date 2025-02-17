48ebf8517aa5f8934734636a91db864f3255ada5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-17 03:12:00 +0000

    Deploy

---
+++ pages/whatsapp/whats.php
@@ -0,0 +1,48 @@
+<?php
+
+require __DIR__ . '/twilio/src/Twilio/autoload.php'; // Ajuste o caminho, se necessário.
+include_once "../../objects/objects.php";
+
+$siteAdmin = new SITE_ADMIN();  
+$siteAdmin->conexao();
+$siteAdmin->getParameterInfo();
+
+$parametros = ['WHATSAPP_TOKEN' => null, 'WHATSAPP_SID' => null, 'WHATSAPP_STATUS' => null];
+
+foreach ($siteAdmin->ARRAY_PARAMETERINFO as $item) {
+    if (array_key_exists($item['CFG_DCPARAMETRO'], $parametros)) {
+        $parametros[$item['CFG_DCPARAMETRO']] = $item['CFG_DCVALOR'];
+    }
+}
+
+// Suas credenciais do Twilio
+$twilioNumber = 'whatsapp:+15557337654'; // Número do Twilio Sandbox
+$token = $parametros['WHATSAPP_TOKEN'];
+$sid = $parametros['WHATSAPP_SID'];
+$statusWhatsapp = $parametros['WHATSAPP_STATUS'];
+$to = "whatsapp:+5511982734350";
+
+
+$client = new Client($sid, $token);
+
+// Usando o template aprovado
+$message = $client->messages->create(
+    $to, // Número de destino com WhatsApp
+    [
+        'from' => $twilioNumber, // Número Twilio
+        'body' => 'Mensagem personaliada se não for usar template',
+        'template' => [
+            'name' => 'prq_hortensias_condominio_encomenda', // Nome do template aprovado
+            'parameters' => [
+                ['type' => 'text', 'text' => $nome],          // usuario_nome = Carlos
+                ['type' => 'text', 'text' => $codigo],         // id_entrega = 123456
+                ['type' => 'text', 'text' => 'Condomínio Parque das Hortênsias'], // condominio_nome = Residencial Alpha
+                ['type' => 'text', 'text' => $link] // link_liberar_entrega
+            ]
+        ]
+    ]
+);
+$resultWhatsTwilioSender =  $message->sid;
+var_dump($resultWhatsTwilioSender);
+
+?>
