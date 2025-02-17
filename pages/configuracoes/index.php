baf52751fd0ba5361aed486569816ddd21c3adde
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-17 20:05:11 +0000

    Deploy

--- pages/configuracoes/index.php
+++ pages/configuracoes/index.php
@@ -123,7 +123,7 @@ if (session_status() === PHP_SESSION_NONE) {
                                                   "nomeCondominio" => ["label" => "Nome do Condomínio", "pattern" => ".*", "maxlength" => "28", "value" => $nomeCondominio],
                                                   "qtdeUnidades" => ["label" => "Quantidade de Unidades", "pattern" => "^[0-9]{1,4}$", "maxlength" => "4", "value" => $qtdeUnidades],
                                                   "email" => ["label" => "E-mail para Notificações", "pattern" => ".*", "maxlength" => "50", "value" => $email],
-                                                  "whatsStatus" => ["label" => "Whatsapp Status", "pattern" => "^[0-9]*$", "maxlength" => "10", "value" => $whatsStatus],
+                                                  "whatsStatus" => ["label" => "Whatsapp Status (ATIVO INATIVO)", "pattern" => "^[0-9]*$", "maxlength" => "10", "value" => $whatsStatus],
                                                   "whatsSender" => ["label" => "Whatsapp Telefone Sender", "pattern" => "^\d{11}$", "maxlength" => "11", "value" => $whatsSender],
                                                   "whatsSid" => ["label" => "Whatsapp SID", "pattern" => ".*", "maxlength" => "50", "value" => $whatsSid],
                                                   "whatsToken" => ["label" => "Whatsapp Token", "pattern" => "^\d{11}$", "maxlength" => "11", "value" => $whatsToken],
