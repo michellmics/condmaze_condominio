533bb3d883630f8e1aa3afc8d520db546c4463a6
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-19 02:29:18 +0000

    Deploy

---
+++ pages/login/processa_recuperacao.php
@@ -0,0 +1,94 @@
+<?php
+include_once 'objetos.php'; 
+
+class RecSystem extends SITE_ADMIN
+{
+    public function CheckValidUser($USU_DCAPARTAMENTO)
+    {
+            if (!$this->pdo) {
+                $this->conexao();
+            }
+
+            $sql = "SELECT USU_DCEMAIL FROM USU_USUARIO WHERE USU_DCAPARTAMENTO = :USU_DCAPARTAMENTO";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindParam(':USU_DCAPARTAMENTO', $USU_DCAPARTAMENTO, PDO::PARAM_STR);
+            $stmt->execute();
+
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            if ($user['USU_DCEMAIL']) 
+            {
+                $email = $user['USU_DCEMAIL'];
+                return $email;             
+            } else 
+                  {
+                    //--------------------LOG----------------------//
+                    $LOG_DCTIPO = "REC SENHA";
+                    $LOG_DCMSG = "Apartamento não cadastrado no sistema";
+                    $LOG_DCUSUARIO = "N/A";
+                    $LOG_DCAPARTAMENTO = $USU_DCAPARTAMENTO;
+                    $this->insertLogInfo($LOG_DCTIPO, $LOG_DCMSG, $LOG_DCUSUARIO, $LOG_DCAPARTAMENTO);
+                    //--------------------LOG----------------------//
+                    echo "Apartamento não cadastrado no sistema. Entre em contato com o síndico.";
+                    exit();
+                  }        
+    }
+
+    public function sendToken($apartamento,$email)
+    {
+            if (!$this->pdo) {
+                $this->conexao();
+            }
+            
+            try 
+            {
+                $USU_DCREDEF_TOKEN = bin2hex(random_bytes(16));
+                $USU_DTREDEF_TOKEN_EXP = date('Y-m-d H:i:s', strtotime('+1 hour'));
+
+                $sql = "UPDATE USU_USUARIO 
+                    SET 
+                    USU_DCREDEF_TOKEN = :USU_DCREDEF_TOKEN, 
+                    USU_DTREDEF_TOKEN_EXP = :USU_DTREDEF_TOKEN_EXP
+                    WHERE USU_DCAPARTAMENTO = :USU_DCAPARTAMENTO";
+
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->bindParam(':USU_DCAPARTAMENTO', $apartamento, PDO::PARAM_STR);
+                $stmt->bindParam(':USU_DCREDEF_TOKEN', $USU_DCREDEF_TOKEN, PDO::PARAM_STR);
+                $stmt->bindParam(':USU_DTREDEF_TOKEN_EXP', $USU_DTREDEF_TOKEN_EXP, PDO::PARAM_STR);
+                $stmt->execute();
+
+                // Enviar o link de redefinição
+                $link = "https://prqdashortensias.com.br/redefinir_senha.php?token=$USU_DCREDEF_TOKEN";
+                $mensagem = "Clique no link para redefinir sua senha: $link";
+                $assunto = "Condomínio Parque das Hortênsias - Recuperação de senha";
+
+                $this->notifyUsuarioEmail($assunto,$mensagem,$email);
+                echo "Um link de recuperação foi enviado para seu e-mail."; 
+                
+                //--------------------LOG----------------------//
+                $LOG_DCTIPO = "REC SENHA";
+                $LOG_DCMSG = "Envio de link de recuperação de senha para o e-mail: $email.";
+                $LOG_DCUSUARIO = "N/A";
+                $LOG_DCAPARTAMENTO = $apartamento;
+                $this->insertLogInfo($LOG_DCTIPO, $LOG_DCMSG, $LOG_DCUSUARIO, $LOG_DCAPARTAMENTO);
+                //--------------------LOG----------------------//
+            }
+            catch (Exception $e) 
+            {
+                echo "Erro ao processar a solicitação";
+            }              
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{
+    $apartamento = $_POST['apartamento'];
+
+    $RecSystem = new RecSystem();
+    
+    $email=$RecSystem->CheckValidUser($apartamento);
+    $result=$RecSystem->sendToken($apartamento,$email);
+
+}
+ 
+?>
---
+++ pages/login/redefinir_senha.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Condomínio Parque das Hortênsias</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.2 -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Font Awesome Icons -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <!-- iCheck -->
+    <link href="plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
+
+		<link rel="icon" href="https://www.prqdashortensias.com.br/logo_icon.ico" type="image/x-icon">
+    	<link rel="shortcut icon" href="https://www.prqdashortensias.com.br/logo_icon.ico" type="image/x-icon">
+    	<link rel="apple-touch-icon" href="https://www.prqdashortensias.com.br/logo_icon.png">
+    	<meta name="apple-mobile-web-app-title" content="Hortensias">
+    	<meta name="apple-mobile-web-app-capable" content="yes">
+    	<meta name="apple-mobile-web-app-status-bar-style" content="default">
+
+      <style>
+      .password-container {
+        position: relative;
+      }
+      .toggle-password {
+        position: absolute;
+        top: 50%;
+        right: 10px;
+        transform: translateY(-50%);
+        cursor: pointer;
+      }
+    </style>
+  </head>
+  <body class="login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <img src="https://www.prqdashortensias.com.br/img/logo_site_small.png"></img>
+      </div><!-- /.login-logo -->
+      <div class="login-box-body">
+        <p class="login-box-msg">Redefinição de senha</p>
+        <form id="demo-form" action="atualizar_senha.php" method="post">
+          <input type="hidden" name="token" value="<?php echo htmlspecialchars($_GET['token']); ?>">
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" id="password" placeholder="Digite sua nova senha" name="password"/>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            <span class="toggle-password" onclick="togglePassword('password_redit', this)"></span>
+            Preencha para alterar a senha. Ela deve ter pelo menos 8 caracteres, incluir uma letra maiúscula e um caractere especial.
+          </div>
+
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" id="password_redit" placeholder="Repita a nova senha" name="password_redit"/>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            <span class="toggle-password" onclick="togglePassword('password_redit', this)"></span>
+          </div>
+          
+          <div class="row">
+            <div class="col-xs-8">                 
+            </div><!-- /.col -->
+            <div class="col-xs-4">
+              
+              <button type="submit" class="btn btn-primary btn-block btn-flat">Enviar</button>
+            </div><!-- /.col -->
+          </div>
+          <br>
+        </form>
+
+						<div id="password-error"></div>
+            <div id="form-message"></div>
+
+<script>
+    function togglePassword(inputId, toggleElement) {
+    const passwordField = document.getElementById(inputId);
+
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+    } else {
+      passwordField.type = "password";
+    }
+  }
+
+    document.getElementById('demo-form').addEventListener('submit', function (e) {
+        e.preventDefault(); // Impede o envio tradicional do formulário
+        
+        const password = document.getElementById('password').value;
+        const passwordRedit = document.getElementById('password_redit').value;
+        const passwordError = document.getElementById('password-error');
+
+        // Verifica se as senhas coincidem
+        if (password !== passwordRedit) {
+            passwordError.style.display = 'block';
+            passwordError.textContent = "As senhas não coincidem.";
+            return; // Interrompe o envio
+        }
+
+        // Verifica os requisitos de força da senha
+        const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{6,}$/;
+        if (!passwordRegex.test(password)) {
+            passwordError.style.display = 'block';
+            passwordError.textContent = "A senha deve ter 6 caracteres, incluindo uma letra maiúscula e um caractere especial.";
+            return; // Interrompe o envio
+        }
+
+        // Se tudo estiver correto, oculta o erro e envia o formulário
+        passwordError.style.display = 'none';
+
+        // Envia o formulário usando Ajax
+        const formData = new FormData(this);
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', this.action, true);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                document.getElementById('form-message').innerHTML = xhr.responseText;
+            } else {
+                document.getElementById('form-message').innerHTML = "Houve um erro no envio do formulário.";
+            }
+        };
+        xhr.send(formData);
+    });
+</script>
+
+
+      </div><!-- /.login-box-body -->
+    </div><!-- /.login-box -->
+    <!-- jQuery 2.1.3 -->
+    <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- iCheck -->
+    <script src="plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+  </body>
+</html>
