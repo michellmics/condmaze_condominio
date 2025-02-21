6c382a7ccb83d7c851a786c24d1cdd38aaedd73f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-21 14:19:57 +0000

    Deploy

--- cronTasks/cron_checkVagasEstacionamento.php
+++ cronTasks/cron_checkVagasEstacionamento.php
@@ -2,5 +2,7 @@
 
 $url = "https://parquedashortensias.codemaze.com.br/pages/ctrlVagasVisitante/service_cron_check_vagas.php"; 
 $conteudo = file_get_contents($url);
+var_dump($conteudo);
+echo $conteudo;
 ?>
 
