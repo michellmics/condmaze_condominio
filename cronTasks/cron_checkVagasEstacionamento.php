c2c2cf02bec7fe38605127d9726109a3ba1bb866
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-21 02:16:57 +0000

    Deploy

--- cronTasks/cron_checkVagasEstacionamento.php
+++ cronTasks/cron_checkVagasEstacionamento.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);        // Reporta todos os erros
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
 $host = $_SERVER['HTTP_HOST'];
 
-include_once "../objects/objects.php";
+require "../objects/objects.php";
 $siteAdmin = new SITE_ADMIN();  
 
 /*
