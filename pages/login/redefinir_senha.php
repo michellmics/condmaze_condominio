a9dae7d78608c8c60d719cfab40b644ec277ff61
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-21 18:23:16 +0000

    Deploy

--- pages/login/redefinir_senha.php
+++ pages/login/redefinir_senha.php
@@ -5,20 +5,17 @@
     <title>Condomínio Parque das Hortênsias</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <!-- Font Awesome Icons -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
-    <link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
     <!-- iCheck -->
-    <link href="plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
+    <link href="../../plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
 
-		<link rel="icon" href="https://www.prqdashortensias.com.br/logo_icon.ico" type="image/x-icon">
-    	<link rel="shortcut icon" href="https://www.prqdashortensias.com.br/logo_icon.ico" type="image/x-icon">
-    	<link rel="apple-touch-icon" href="https://www.prqdashortensias.com.br/logo_icon.png">
-    	<meta name="apple-mobile-web-app-title" content="Hortensias">
-    	<meta name="apple-mobile-web-app-capable" content="yes">
-    	<meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <!-- PWA MOBILE CONF -->
+    <?php include '../../src/pwa_conf.php'; ?>
+	<!-- PWA MOBILE CONF -->
 
       <style>
       .password-container {
@@ -36,7 +33,7 @@
   <body class="login-page">
     <div class="login-box">
       <div class="login-logo">
-        <img src="https://www.prqdashortensias.com.br/img/logo_site_small.png"></img>
+        <img src="https://parquedashortensias.codemaze.com.br/img/logo_site_small.png"></img>
       </div><!-- /.login-logo -->
       <div class="login-box-body">
         <p class="login-box-msg">Redefinição de senha</p>
@@ -124,10 +121,10 @@
       </div><!-- /.login-box-body -->
     </div><!-- /.login-box -->
     <!-- jQuery 2.1.3 -->
-    <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
+    <script src="../../plugins/jQuery/jQuery-2.1.3.min.js"></script>
     <!-- Bootstrap 3.3.2 JS -->
-    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <!-- iCheck -->
-    <script src="plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+    <script src="../../plugins/iCheck/icheck.min.js" type="text/javascript"></script>
   </body>
 </html>
