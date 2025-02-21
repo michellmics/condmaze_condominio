d8fd427e574e05b9d7486dff2e6cf1409fbd0d55
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-21 11:22:17 +0000

    Deploy

--- cronTasks/cron_checkVagasEstacionamento.php
+++ cronTasks/cron_checkVagasEstacionamento.php
@@ -10,7 +10,7 @@ include_once "../objects/objects.php";
 
 $siteAdmin = new SITE_ADMIN();  
 
-
+/*
 
 // Função para calcular a diferença de tempo entre agora e o 'entry_time'
 function checkForAlarm($entry_time) {
@@ -23,28 +23,29 @@ function checkForAlarm($entry_time) {
     // Verifica se a diferença é maior que 24 horas
     return $interval->days > 2;
 }
-
+*/
 // Caminho do arquivo JSON
-$json_file = '../pages/ctrlVagasVisitante/vagas/slots.json';
+//$json_file = '../pages/ctrlVagasVisitante/vagas/slots.json';
 
 // Lê o arquivo JSON
-$slots = json_decode(file_get_contents($json_file), true); 
+//$slots = json_decode(file_get_contents($json_file), true); 
 
 // Itera sobre os slots
 //foreach ($slots as $id => $slot) {
     // Verifica se há um tempo de entrada
    // if (!empty($slot['entry_time']) && checkForAlarm($slot['entry_time'])) {
         // Se a diferença for maior que 48h, define o 'alarm' como 'alarmed'
-        $slots[$id]['alarm'] = 'alarmed';
+        //$slots[$id]['alarm'] = 'alarmed';
 
-        echo "Encontrado veiculo irregular.";
+        //echo "Encontrado veiculo irregular.";
 
-        $veiculoI = $slot['vehicle_model']; 
-        $placaI = $slot['plate']; 
-        $apartamentoI = $slot['apartment'];
+        //$veiculoI = $slot['vehicle_model']; 
+        //$placaI = $slot['plate']; 
+        //$apartamentoI = $slot['apartment'];
 
         $ASSUNTO = "ALERTA DE VEÍCULO IRREGULAR";
-        $MSG = "O veículo modelo $veiculoI com placa $placaI sob responsabilidade do apartamento $apartamentoI está no estacionamento de visitantes a mais de 48h.";
+       // $MSG = "O veículo modelo $veiculoI com placa $placaI sob responsabilidade do apartamento $apartamentoI está no estacionamento de visitantes a mais de 48h.";
+        $MSG = "O veículo modelo com placa  sob responsabilidade do apartamento  está no estacionamento de visitantes a mais de 48h.";
         $EMAIL = "suporte@codemaze.com.br";
         $sendMailResult = $siteAdmin->notifyUsuarioEmail($ASSUNTO, $MSG, $EMAIL);
 
@@ -53,12 +54,12 @@ $slots = json_decode(file_get_contents($json_file), true);
         //die();
    // }
 //}
-
+/*
 // Salva as alterações no arquivo JSON
 file_put_contents($json_file, json_encode($slots, JSON_PRETTY_PRINT));
 
 echo "Arquivo JSON atualizado com sucesso.";
-
+*/
 
 
 
