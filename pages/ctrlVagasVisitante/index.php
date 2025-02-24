3a4e205a3c8eedb5be11a851a033e1dd8c946f52
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-24 14:46:00 +0000

    Deploy

--- pages/ctrlVagasVisitante/index.php
+++ pages/ctrlVagasVisitante/index.php
@@ -1,8 +1,5 @@
 
 <?php
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
     if (!isset($_SESSION['user_id']) || $_SESSION['user_id'] == NULL) {
         header("Location: ../login/index.php");
         exit();
