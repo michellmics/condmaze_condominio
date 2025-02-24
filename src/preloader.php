abace95919f744dd94274b76fc87d51ab584150b
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-24 00:38:14 +0000

    Deploy

--- src/preloader.php
+++ src/preloader.php
@@ -6,54 +6,35 @@
 
 <style>
 /* Preloader container */
-#preloader {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(255, 255, 255, 0.8);  /* semi-transparent background */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 9999;  /* ensures it's on top of everything */
-}
-
-/* Loader styles */
 .loader {
-  width: 64px;
-  height: 64px;
+  width: 48px;
+  height: 48px;
+  display: inline-block;
   position: relative;
-  animation: rotate 1.5s ease-in infinite alternate;
 }
-
+.loader::after,
 .loader::before {
-  content: '';
+  content: '';  
+  box-sizing: border-box;
+  width: 48px;
+  height: 48px;
+  border: 2px solid #ae00ff;
   position: absolute;
   left: 0;
-  bottom: 0;
-  color:rgb(255, 36, 65);
-  background: currentColor;
-  width: 64px;
-  height: 32px;
-  border-radius: 0 0 50px 50px;
+  top: 0;
+  animation: rotation 2s ease-in-out infinite alternate;
 }
-
 .loader::after {
-  content: '';
-  position: absolute;
-  left: 50%;
-  top: 10%;
-  background: rgb(22, 109, 150);
-  width: 8px;
-  height: 64px;
-  animation: rotate 1.2s linear infinite alternate-reverse;
+  border-color:rgb(56, 1, 82);
+  animation-direction: alternate-reverse;
 }
 
-/* Rotation animation */
-@keyframes rotate {
-  100% { 
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
     transform: rotate(360deg);
   }
-}
+} 
 </style>  
