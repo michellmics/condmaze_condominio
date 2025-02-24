0d99aeb18eb42470bcbcd02e7e81c6ac8ef343f2
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-24 18:25:36 +0000

    Deploy

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -260,13 +260,8 @@
 
                         </div> <!-- end col -->
                     </div>
-                    <!-- end row -->
-
-
-
-                                                    
-                </div><!-- FIM CONTEUDO CONTAINER -->                
-
+                    <!-- end row -->                                                    
+                </div><!-- FIM CONTEUDO CONTAINER -->              
             <!-- content -->
         <!-- FOOTER -->
 	    <?php include '../../src/footerNav.php'; ?>
@@ -297,21 +292,6 @@
 
     <!-- App js -->
     <script src="../../assets/js/app.min.js"></script>
-
-    <!-- Datatables js -->
-    <script src="../../assets/vendor/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-fixedcolumns-bs5/js/fixedColumns.bootstrap5.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-buttons/js/buttons.flash.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-    <script src="../../assets/vendor/datatables.net-select/js/dataTables.select.min.js"></script>
 </body>
 
 </html>
