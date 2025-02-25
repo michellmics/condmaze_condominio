e8909cc59bb468dba118743b8da1ffd1d6934f48
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-25 17:18:48 +0000

    Deploy

--- src/topBar.php
+++ src/topBar.php
@@ -44,7 +44,7 @@
                     <!-- Topbar Brand Logo -->
                     <div class="logo-topbar">
                         <!-- Logo light -->
-                        <a href="../pages/inicial/index.php" class="logo-light">
+                        <a href="../inicial/index.php" class="logo-light">
                             <span class="logo-lg">
                                 <img src="../../img/logo_128x32_white.png" alt="logo">
                             </span>
@@ -54,7 +54,7 @@
                         </a>
 
                         <!-- Logo Dark -->
-                        <a href="../pages/inicial/index.php" class="logo-dark">
+                        <a href="../inicial/index.php" class="logo-dark">
                             <span class="logo-lg">
                                 <img src="../../img/logo_128x32_black.png" alt="dark logo">
                             </span>
