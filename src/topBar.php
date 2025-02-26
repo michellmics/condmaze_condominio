2e7ecba4b40b430e361c218e5e44122d7c3e54d0
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-26 01:26:10 +0000

    Deploy

--- src/topBar.php
+++ src/topBar.php
@@ -151,6 +151,7 @@
                              
                                 <a href="javascript:void(0);" class="dropdown-item p-0 notify-item card unread-noti shadow-none mb-2">
                                     <div class="card-body">
+                                        
                                         <span class="float-end noti-close-btn text-muted"><i class="mdi mdi-close"></i></span>
                                         <div class="d-flex align-items-center">
                                             <div class="flex-shrink-0">
@@ -158,18 +159,26 @@
                                                     <i class="mdi mdi-comment-account-outline"></i>
                                                 </div>
                                             </div>
-                                            
                                             <div class="flex-grow-1 text-truncate ms-2">
                                                 <h5 class="noti-item-title fw-semibold font-14">Datacorp <small class="fw-normal text-muted ms-1">1 min ago</small></h5>
                                                 <small class="noti-item-subtitle text-muted">Caleb Flakelar commented on Admin</small>
                                             </div>
-
-                                            <div class="flex-grow-1 text-truncate ms-2">
-                                                <h5 class="noti-item-title fw-semibold font-14">Datacorp <small class="fw-normal text-muted ms-1">1 min ago</small></h5>
-                                                <small class="noti-item-subtitle text-muted">Caleb Flakelar commented on Admin</small>
-                                            </div>
-
                                         </div>
+
+                                        <span class="float-end noti-close-btn text-muted"><i class="mdi mdi-close"></i></span>
+                                        <div class="d-flex align-items-center">
+                                            <div class="flex-shrink-0">
+                                                <div class="notify-icon bg-primary">
+                                                    <i class="mdi mdi-comment-account-outline"></i>
+                                                </div>
+                                            </div>
+                                            <div class="flex-grow-1 text-truncate ms-2">
+                                                <h5 class="noti-item-title fw-semibold font-14">Datacorp <small class="fw-normal text-muted ms-1">1 min ago</small></h5>
+                                                <small class="noti-item-subtitle text-muted">Caleb Flakelar commented on Admin</small>
+                                            </div>
+                                        </div>
+
+
                                     </div>
                                 </a>
 
