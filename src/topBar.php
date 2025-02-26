c279a54ab85ea7dacc28f5da8c56839009b0e0ab
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-26 12:50:21 +0000

    Deploy

--- src/topBar.php
+++ src/topBar.php
@@ -162,7 +162,7 @@
                                     <div class="card-body">
                                         
                                         <span class="float-end noti-close-btn text-muted" 
-                                              data-id="<?= htmlspecialchars($notificacaoItem['USN_IDNOTIFICACAO']); ?>">
+                                              >
                                             <i class="mdi mdi-close"></i>
                                         </span>
                                         <div class="d-flex align-items-center">
