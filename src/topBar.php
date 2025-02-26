33f122aa5a198af812f38687755d07160198718b
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-26 02:40:40 +0000

    Deploy

--- src/topBar.php
+++ src/topBar.php
@@ -170,7 +170,7 @@
                                             </div>
                                             <div class="flex-grow-1 text-truncate ms-2">
                                                 <h5 class="noti-item-title fw-semibold font-14"><?= htmlspecialchars($notificacaoItem['NOT_DCTITLE']); ?> <small class="fw-normal text-muted ms-1"><?= htmlspecialchars($dataFormatPend); ?></small></h5>
-                                                <small class="noti-item-subtitle text-muted"><?= htmlspecialchars($notificacaoItem['NOT_DCMSG']); ?></small>
+                                                <small title="Modo do Tema" class="noti-item-subtitle text-muted"><?= htmlspecialchars($notificacaoItem['NOT_DCMSG']); ?></small>
                                             </div>
                                         </div>
                                     </div>
