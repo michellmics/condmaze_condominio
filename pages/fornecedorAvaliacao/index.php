ab425e1996174faa243cf30bcca3c5257891fc8b
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-27 12:16:23 +0000

    Deploy

--- pages/fornecedorAvaliacao/index.php
+++ pages/fornecedorAvaliacao/index.php
@@ -217,14 +217,15 @@
                                                 ?>
                                                 <div class="accordion-item">                                                    
                                                     <h2 class="accordion-header" id="headingOne">
-                                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#pedreiro<?php echo $aux; ?>" aria-expanded="true" aria-controls="collapse<?php echo $aux; ?>" style="display: flex; justify-content: space-between; align-items: center; text-align: left;">
-                                                        <div style="display: flex; flex-direction: column; color: #7091F5;">
-                                                            <strong><?= htmlspecialchars($item['PDS_DCNOME']); ?></strong>
-                                                            <span>Fone: <?= htmlspecialchars(preg_replace('/^(\d{2})(\d{5})(\d{4})$/', '$1-$2-$3', $item['PDS_DCTELEFONE'])); ?></span><span> <?= htmlspecialchars(mb_convert_case(mb_strtolower($item['PDS_DCCIDADE'], 'UTF-8'), MB_CASE_TITLE, 'UTF-8')); ?></span>
-                                                        </div>                                
-                                                        <div class="rateit rateit-mdi" data-rateit-mode="font" data-rateit-icon="󰓒" data-rateit-value="<?= htmlspecialchars($NOTASAVG['AVG']); ?>" data-rateit-ispreset="true" data-rateit-readonly="true" style="margin-left: auto;"></div>
+                                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#pedreiro<?php echo $aux; ?>" aria-expanded="true" aria-controls="collapse<?php echo $aux; ?>" style="display: flex; justify-content: space-between; align-items: center; text-align: left; width: 100%; padding: 0; min-height: 20%;">
+    
+                                                        <div style="flex: 1; display: flex; align-items: center; justify-content: center; background-color: black;">
+                                                        <img src="../../publicidade/img/camp_barao_pneus.gif" alt="Descrição da imagem" 
+                                                        style="max-width: 100%; height: auto; display: block;">
+                                                        </div>
+                                                                                                    
                                                         <?php if ($nivelAcesso == 'SINDICO'): ?>
-                                                            <i class="mdi mdi-delete" title="Excluir Prestador" style="cursor: pointer; font-size: 24px;" onclick="confirmDeletePrestador(event, '<?php echo htmlspecialchars($item['PDS_IDPRESTADOR_SERVICO'], ENT_QUOTES, 'UTF-8'); ?>')"></i>                                                            
+                                                            <i class="mdi mdi-delete" title="Excluir Prestador" style="cursor: pointer; font-size: 24px; margin-right: 10px;" onclick="confirmDeletePrestador(event, '<?php echo htmlspecialchars($item['PDS_IDPRESTADOR_SERVICO'], ENT_QUOTES, 'UTF-8'); ?>')"></i>                                                            
                                                         <?php endif; ?>
                                                     </button>
                                                     </h2>
@@ -293,14 +294,15 @@
                                                 ?>
                                                 <div class="accordion-item">                                                    
                                                     <h2 class="accordion-header" id="headingOne">
-                                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#gesso<?php echo $aux; ?>" aria-expanded="true" aria-controls="collapse<?php echo $aux; ?>" style="display: flex; justify-content: space-between; align-items: center; text-align: left;">
-                                                        <div style="display: flex; flex-direction: column; color: #7091F5;">
-                                                            <strong><?= htmlspecialchars($item['PDS_DCNOME']); ?></strong>
-                                                            <span>Fone: <?= htmlspecialchars(preg_replace('/^(\d{2})(\d{5})(\d{4})$/', '$1-$2-$3', $item['PDS_DCTELEFONE'])); ?></span><span> <?= htmlspecialchars(mb_convert_case(mb_strtolower($item['PDS_DCCIDADE'], 'UTF-8'), MB_CASE_TITLE, 'UTF-8')); ?></span>
-                                                        </div>                                
-                                                        <div class="rateit rateit-mdi" data-rateit-mode="font" data-rateit-icon="󰓒" data-rateit-value="<?= htmlspecialchars($NOTASAVG['AVG']); ?>" data-rateit-ispreset="true" data-rateit-readonly="true" style="margin-left: auto;"></div>
+                                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#gesso<?php echo $aux; ?>" aria-expanded="true" aria-controls="collapse<?php echo $aux; ?>" style="display: flex; justify-content: space-between; align-items: center; text-align: left; width: 100%; padding: 0; min-height: 20%;">
+    
+                                                        <div style="flex: 1; display: flex; align-items: center; justify-content: center; background-color: black;">
+                                                        <img src="../../publicidade/img/camp_dzsports.gif" alt="Descrição da imagem" 
+                                                        style="max-width: 100%; height: auto; display: block;">
+                                                        </div>
+                                                                                                    
                                                         <?php if ($nivelAcesso == 'SINDICO'): ?>
-                                                            <i class="mdi mdi-delete" title="Excluir Prestador" style="cursor: pointer; font-size: 24px;" onclick="confirmDeletePrestador(event, '<?php echo htmlspecialchars($item['PDS_IDPRESTADOR_SERVICO'], ENT_QUOTES, 'UTF-8'); ?>')"></i>                                                            
+                                                            <i class="mdi mdi-delete" title="Excluir Prestador" style="cursor: pointer; font-size: 24px; margin-right: 10px;" onclick="confirmDeletePrestador(event, '<?php echo htmlspecialchars($item['PDS_IDPRESTADOR_SERVICO'], ENT_QUOTES, 'UTF-8'); ?>')"></i>                                                            
                                                         <?php endif; ?>
                                                     </button>
                                                     </h2>
