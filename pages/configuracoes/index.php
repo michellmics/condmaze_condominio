6ed3138f7fb9ca87e263961338fca0e2ee3b300a
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-03-03 05:46:02 +0000

    Deploy

--- pages/configuracoes/index.php
+++ pages/configuracoes/index.php
@@ -129,7 +129,7 @@ if (!in_array(strtoupper($_SESSION['user_nivelacesso']), ["SINDICO", "SUPORTE"])
                                                   "email" => ["label" => "E-mail para Notificações", "pattern" => ".*", "maxlength" => "50", "value" => $email],
                                                   "whatsStatus" => ["label" => "Whatsapp Status (ATIVO INATIVO)", "pattern" => "^[0-9]*$", "maxlength" => "10", "value" => $whatsStatus],
                                                   "whatsSindico" => ["label" => "Whatsapp Telefone Síndico", "pattern" => ".*", "maxlength" => "20", "value" => $whatsSindico],
-                                                  "whatsportaria" => ["label" => "Whatsapp Telefone Síndico", "pattern" => ".*", "maxlength" => "20", "value" => $whatsportaria],
+                                                  "whatsportaria" => ["label" => "Whatsapp Telefone Portaria", "pattern" => ".*", "maxlength" => "20", "value" => $whatsportaria],
                                                   "whatsEndpoint" => ["label" => "Whatsapp Token", "pattern" => ".*", "maxlength" => "20", "value" => $whatsEndpoint],
                                                   "whatsInstancia" => ["label" => "Whatsapp SID", "pattern" => ".*", "maxlength" => "50", "value" => $whatsInstancia],
                                                   "whatsToken" => ["label" => "Whatsapp Token", "pattern" => ".*", "maxlength" => "50", "value" => $whatsToken],
