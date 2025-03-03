c5d687701b4c2de61f3b2078f738ddc9456aa62a
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-03-03 04:39:23 +0000

    Deploy

--- pages/cron/checkWhats.php
+++ pages/cron/checkWhats.php
@@ -7,22 +7,47 @@ $siteAdmin->getParameterInfo();
 foreach ($siteAdmin->ARRAY_PARAMETERINFO as $item) {
     if ($item['CFG_DCPARAMETRO'] == 'TELEFONE_PORTARIA') {
         $telefonePortaria = $item['CFG_DCVALOR']; 
-        break; 
+    }
+    if ($item['CFG_DCPARAMETRO'] == 'EMAIL_ALERTAS') {
+        $emailAlerta = $item['CFG_DCVALOR']; 
     }
 }  
+$status = 0;
 
 $MSG = "Olá, bom dia. Checagem de conexão com o Whatsapp concluída. *Operacional*";
 $response = $siteAdmin->whatsappApiSendMessage($MSG, $telefonePortaria);
 
-//--------------------LOG----------------------//
-$LOG_DCTIPO = "NOTIFICAÇÃO";
-$LOG_DCMSG = "Checagem do Status da API do Whatsapp concluída com sucesso.";
-$LOG_DCUSUARIO = "SISTEMA";
-$LOG_DCCODIGO = "N/A";
-$LOG_DCAPARTAMENTO = "N/A";
-$siteAdmin->insertLogInfo($LOG_DCTIPO, $LOG_DCMSG, $LOG_DCUSUARIO, $LOG_DCAPARTAMENTO, $LOG_DCCODIGO);
-//--------------------LOG----------------------//
+if (isset($response['status'])) {
+    $status = $data['status']; // Captura apenas o status
+}
 
-var_dump($response);
+if($status == "500")
+{    
+    //--------------------LOG----------------------//
+    $LOG_DCTIPO = "NOTIFICAÇÃO";
+    $LOG_DCMSG = "Checagem do Status da API do Whatsapp concluída com erro. Verifique a conexão com a API.";
+    $LOG_DCUSUARIO = "SISTEMA";
+    $LOG_DCCODIGO = "N/A";
+    $LOG_DCAPARTAMENTO = "N/A";
+    $siteAdmin->insertLogInfo($LOG_DCTIPO, $LOG_DCMSG, $LOG_DCUSUARIO, $LOG_DCAPARTAMENTO, $LOG_DCCODIGO);
+    //--------------------LOG----------------------//
+
+    $siteAdmin->insertNotificacaoFrontByUsuario("API Whatsapp", "Notificação está Offline", "1000");
+    $SUBJECT = "ATENÇÃO - NOTIFICAÇÃO POR WHATSAPP ESTÁ OFFINE";
+    $siteAdmin->notifyUsuarioEmail($SUBJECT, $LOG_DCMSG, $emailAlerta, $anexo="na");
+
+}
+else
+    {
+        //--------------------LOG----------------------//
+        $LOG_DCTIPO = "NOTIFICAÇÃO";
+        $LOG_DCMSG = "Checagem do Status da API do Whatsapp concluída com sucesso.";
+        $LOG_DCUSUARIO = "SISTEMA";
+        $LOG_DCCODIGO = "N/A";
+        $LOG_DCAPARTAMENTO = "N/A";
+        $siteAdmin->insertLogInfo($LOG_DCTIPO, $LOG_DCMSG, $LOG_DCUSUARIO, $LOG_DCAPARTAMENTO, $LOG_DCCODIGO);
+        //--------------------LOG----------------------//
+    }
+
 
 ?>
