a77cce545e7a791d50a8745b59bb16f1a550c72e
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-03-03 22:08:38 +0000

    Deploy

--- assets/js/blockCode.js
+++ assets/js/blockCode.js
@@ -1,10 +1,9 @@
 document.addEventListener("keydown", function(event) {
-    // Bloquear teclas que abrem as DevTools e o menu de contexto
     if (event.key === "F12" || 
         (event.ctrlKey && event.shiftKey && event.key === "I") || 
         (event.ctrlKey && event.shiftKey && event.key === "J") || 
-        (event.ctrlKey && event.key === "U") || 
-        (event.ctrlKey && (event.key === "S" || event.key === "P"))) {
+        (event.ctrlKey && event.key === "U")) {
+        
         event.preventDefault();
     }
 });
@@ -13,48 +12,4 @@ document.addEventListener("contextmenu", function(event) {
     event.preventDefault();
 });
 
-document.addEventListener("dragstart", function(event) {
-    event.preventDefault();
-});
 
-// Monitorar a visibilidade da página
-document.addEventListener("visibilitychange", function() {
-    if (document.hidden) {
-        alert("Detectamos que você mudou de aba! Ação não permitida.");
-    }
-});
-
-setInterval(() => {
-    console.clear();
-}, 1000);
-
-// Verificar a diferença entre outerWidth e innerWidth
-let checkDevTools = setInterval(function () {
-    let widthDiff = window.outerWidth - window.innerWidth;
-    let heightDiff = window.outerHeight - window.innerHeight;
-
-    // Somente ativar o bloqueio se a diferença for significativa
-    if (widthDiff > 150 || heightDiff > 150) {
-        document.body.innerHTML = "<h1 style='color: red; text-align: center;'>Acesso não autorizado!</h1>";
-        document.body.style.pointerEvents = "none"; 
-        alert("DevTools detectado! Página bloqueada.");
-    }
-}, 2000);
-
-// Detecção mais precisa de DevTools com debugger
-function detectDevTools() {
-    let before = new Date();
-    setInterval(function () {
-        debugger;  // Usar o debugger para detectar se as DevTools estão abertas
-        let after = new Date();
-        
-        // Somente se a pausa no debugger for maior que um intervalo esperado
-        if (after - before > 100) {
-            document.body.innerHTML = "<h1 style='color: red; text-align: center;'>Acesso não autorizado!</h1>";
-            document.body.style.pointerEvents = "none"; 
-            alert("DevTools detectado! Página bloqueada.");
-        }
-    }, 2000);
-}
-
-detectDevTools();
